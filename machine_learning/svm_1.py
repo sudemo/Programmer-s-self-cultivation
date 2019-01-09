ddae937e1f6688b653589b6ac00fa8bf0cc07cd1
neo <zwzhang@colibri.com.cn>
2019-01-09 17:40:32 +0800

    update explanation

--- machine_learning/svm_1.py
+++ machine_learning/svm_1.py
@@ -14,7 +14,17 @@ iris = load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
 
-clf1 = SVC(kernel='poly',gamma='auto')
+"""
+kernel='linear'时，为线性核，C越大分类效果越好，但有可能会过拟合（defaul C=1）。
+
+kernel='rbf'时（default），为高斯核，gamma值越小，分类界面越连续；gamma值越大，分类界面越“散”，
+分类效果越好，但有可能会过拟合。
+
+decision_function_shape='ovr'时，为one v rest，即一个类别与其他类别进行划分，
+
+decision_function_shape='ovo'时，为one v one，即将类别两两之间进行划分，用二分类的方法模拟多分类的结果。
+"""
+clf1 = SVC(kernel='poly',gamma='auto', decision_function_shape='ovr')
 clf1.fit(X, y)
 
 clf2 = LogisticRegression()
