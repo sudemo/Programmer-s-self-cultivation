728bed5069507373e94b5a8c877950819c92f56e
neo <zwzhang@colibri.com.cn>
2019-01-16 10:50:56 +0800

    demo2

---
+++ C#/demo2/demo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hi nerd");
+            Console.ReadKey();
+        }
+    }
+}
