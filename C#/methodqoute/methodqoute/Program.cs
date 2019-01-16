1bfa601eb180e6ad21e05ee64388d759b177d8f0
neo <zwzhang@colibri.com.cn>
2019-01-16 14:56:53 +0800

    find max

---
+++ C#/methodqoute/methodqoute/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//<Access Specifier> <Return Type> <Method Name>(Parameter List)
+//{
+//   Method Body 关于放法的定义
+//}
+namespace methodqoute
+{
+    class findmaxfunc
+    {
+        public int findmax ( int arg1,int arg2)
+        {
+            /*局部变量声明*/
+            int result;
+            if (arg1 > arg2)
+                result = arg1;
+            else
+                result = arg2;
+            return  result;
+        }
+        static void Main(string[] args)
+        {
+           // string ab = Console.ReadLine();
+            Console.WriteLine("plz input");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int  a = Convert.ToInt32(Console.ReadLine());
+            //int a = string ab
+            //int b = string bb;
+            int ret;
+            findmaxfunc aa = new findmaxfunc();
+            //调用
+            ret = aa.findmax(a, b);
+            Console.Write("max: {0}",ret);
+            Console.ReadLine();
+            
+        }
+
+    }
+}
