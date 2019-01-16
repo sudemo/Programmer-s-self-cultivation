00af80eaa91cb0d8cc705b9b65a2170248ad4f92
neo <zwzhang@colibri.com.cn>
2019-01-16 16:37:57 +0800

    字符串处理

---
+++ C#/string处理/string处理/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StringApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //字符串，字符串连接
+            string fname, lname;
+            fname = "Rowan";
+            lname = "Atkinson";
+
+            string fullname = fname + lname;
+            Console.WriteLine("Full Name: {0}", fullname);
+
+            //通过使用 string 构造函数
+            char[] letters = { 'H', 'e', 'l', 'l', 'o' };
+            string greetings = new string(letters);
+            Console.WriteLine("Greetings: {0}", greetings);
+
+            //方法返回字符串
+            string[] sarray = { "Hello", "From", "Tutorials", "Point" };
+            string message = String.Join(" ", sarray);
+            Console.WriteLine("Message: {0}", message);
+
+            //用于转化值的格式化方法
+            DateTime waiting = new DateTime(2012, 10, 10, 17, 58, 1);
+            DateTime t1 = DateTime.Now;
+            Console.WriteLine("现在时间是：{0}",t1);
+            string chat = String.Format("Message sent at {0:t} on {0:D}",
+            waiting);
+            Console.WriteLine("Message: {0}", chat);
+            Console.ReadKey();
+        }
+    }
+}
