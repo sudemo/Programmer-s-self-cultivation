9fd5a6c1159fd5eca8170199cd2353a38fb1c175
neo <zwzhang@colibri.com.cn>
2019-01-16 16:16:31 +0800

    数组遍历的两种方法

--- C#/parameterdelivery/parameterdelivery/Program.cs
+++ C#/parameterdelivery/parameterdelivery/Program.cs
@@ -34,7 +34,35 @@
 //        }
 //    }
 //}
-//////////////以下为///////////////////
+//////////////以下为数组操作///////////////////
+
+//namespace ArrayApplication
+//{
+//    class MyArray
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] n = new int[10]; /* n 是一个带有 10 个整数的数组 */
+//            int i, j;
+
+
+//            /* 初始化数组 n 中的元素 */
+//            for (i = 0; i < 10; i++)
+//            {
+//                n[i] = i + 100;
+//            }
+
+//            /* 输出每个数组元素的值 */
+//            for (j = 0; j < 10; j++)
+//            {
+//                Console.WriteLine("数组中的第[{0}]个元素 = {1}", j, n[j]);
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//}
+///////////////上个例子用的for循环遍历数组，现在使用foreach////////////////////////
+
 
 namespace ArrayApplication
 {
@@ -43,19 +71,19 @@ namespace ArrayApplication
         static void Main(string[] args)
         {
             int[] n = new int[10]; /* n 是一个带有 10 个整数的数组 */
-            int i, j;
 
 
             /* 初始化数组 n 中的元素 */
-            for (i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
                 n[i] = i + 100;
             }
 
             /* 输出每个数组元素的值 */
-            for (j = 0; j < 10; j++)
+            foreach (int j in n)
             {
-                Console.WriteLine("数组中的第[{0}]个元素 = {1}", j, n[j]);
+                int i = j - 100;
+                Console.WriteLine("Element[{0}] = {1}", i, j);
             }
             Console.ReadKey();
         }
