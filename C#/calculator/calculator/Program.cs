13505e7d9cd4d24155b091c900af623e61d6d8f2
neo <zwzhang@colibri.com.cn>
2019-01-16 15:17:13 +0800

    占位符{i}

---
+++ C#/calculator/calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class calculator
+    {
+        public int fact(int num)
+        {
+            int ret;
+            if (num == 1)
+                return 1;
+            else
+                ret = fact(num - 1) * num;
+            return ret;
+         }
+
+
+        static void Main(string[] args)
+        {
+            calculator n = new calculator();
+            //qoute method
+            //while true
+            
+                int c = Convert.ToInt32( Console.ReadLine());
+                Console.WriteLine("{0}的阶乘是：{1}",c, n.fact(c));
+                Console.ReadKey();
+        }  
+    }
+}
