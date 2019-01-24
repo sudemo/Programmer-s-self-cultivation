a807163268575c4a6a2761958b13741a2bf39dd7
neo <zwzhang@colibri.com.cn>
2019-01-24 14:35:28 +0800

    内插表达式

---
+++ C#/solution/内插表达式/内插表达式/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 内插表达式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "<neo>";
+            Console.WriteLine($"thisis{name}.nice to meet u!");
+            /*
+            字符串文本以 $ 字符开头，后接左双引号字符。 
+            $ 符号和引号字符之间不能有空格。
+            一个或多个内插表达式。 左大括号和右大括号（{ 和 }）指示内插表达式。
+            可将任何返回值的 C# 表达式置于大括号内（包括 null）
+            */
+
+
+
+
+
+        }
+    }
+}
