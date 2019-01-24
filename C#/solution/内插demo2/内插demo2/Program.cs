9821eae797d4138921cd46a138e19b95c6f31219
neo <zwzhang@colibri.com.cn>
2019-01-24 17:11:14 +0800

    内插实例

---
+++ C#/solution/内插demo2/内插demo2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 内插demo2
+{
+    public class Vegetable
+    {
+
+
+
+
+
+        public string Name
+        {
+            get;
+        }
+        public Vegetable(string name)
+        {
+            //Console.WriteLine("ji");
+            //这个构造函数存在的问题是：无法使用lambda表达式 必须补充函数的定义 实现部分
+            Name = name;
+        }
+        //public Vegetable(string name) => Name = name;
+        public override string ToString() => Name;
+
+
+        public enum Unit { sitem, kilogram, gram, dozen };
+        static void Main()
+        {
+
+            //Vegetable item = new Vegetable();
+            var ite = new Vegetable("eggplant");
+            var date = DateTime.Now;
+            var price = 1.99m;
+            var unit = Unit.kilogram;
+            Console.WriteLine($"On {date}, the price of {ite} was {price} per {unit}.");
+            Console.ReadKey();
+
+        }
+    }
+}
