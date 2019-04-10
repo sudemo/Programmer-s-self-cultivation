22191b3f16b143d5bfca8248bd2f3d9f1bad7942
neo <zwzhang@colibri.com.cn>
2019-04-10 18:44:16 +0800

    update demo2

---
+++ C#/solution/delegatedemo2/delegatedemo2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace DelegateAppl
+{
+    class PrintString
+    {
+        static FileStream fs;
+        static StreamWriter sw;
+        // 委托声明
+        public delegate void printString(string s);
+
+        // 该方法打印到控制台
+        public static void WriteToScreen(string str)
+        {
+            Console.WriteLine("The String is: {0}", str);
+        }
+        // 该方法打印到文件
+        public static void WriteToFile(string s)
+        {
+            fs = new FileStream("d:\\message.txt",
+            FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            sw.WriteLine(s);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+        // 该方法把委托作为参数，并使用它调用方法
+        public static void sendString(printString ps)
+        {
+            ps("Hello World");
+        }
+        static void Main(string[] args)
+        {
+            printString ps1 = new printString(WriteToScreen);
+            printString ps2 = new printString(WriteToFile);
+            sendString(ps1);
+            sendString(ps2);
+            Console.ReadKey();
+        }
+    }
+}
