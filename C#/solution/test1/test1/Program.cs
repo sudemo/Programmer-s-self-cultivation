7b29c99c33f46a9e84cee9ca0c5cc8b809bf76f4
neo <zwzhang@colibri.com.cn>
2019-04-12 16:22:46 +0800

    update explanation ref

---
+++ C#/solution/test1/test1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        
+    }
+
+    class gump
+    {
+        public double Square(ref double x)
+        {
+            x = x * x;
+            return x;
+        }
+    }
+    class TestApp
+    {
+        public static void Main()
+        {
+            gump doit = new gump();
+            double a = 3;
+            double b = 0;
+            MessageBox.Show(a.ToString());
+            //Console.WriteLine("Before a={0},b={1}", a, b);
+            b = doit.Square(ref a);
+            //Console.WriteLine("After a={0},b={1}", a, b);
+            MessageBox.Show(a.ToString(),"After"); //为什么顺序是反着的呢？
+            //.ReadLine();
+            //Console.ReadKey();
+        }
+    }
+}
