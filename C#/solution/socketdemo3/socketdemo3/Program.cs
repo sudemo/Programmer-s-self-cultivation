0c1f4bd94c9dfa55d7d787d19525c044a70501c1
neo <zwzhang@colibri.com.cn>
2019-04-16 10:58:09 +0800

    update socketdemo3 &server

---
+++ C#/solution/socketdemo3/socketdemo3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//导入的命名空间
+using System.Net.Sockets;
+
+namespace SocketClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //创建一个Socket
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //连接到指定服务器的指定端口
+            //方法参考：http://msdn.microsoft.com/zh-cn/library/system.net.sockets.socket.connect.aspx
+            try
+            {
+                socket.Connect("localhost", 4530);
+
+
+                Console.WriteLine("connect to the server");
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.Read();
+            }
+        }
+            
+
+    }
+}
---
+++ C#/solution/socketdemo3server/socketdemo3server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//额外导入的两个命名空间
+using System.Net.Sockets;
+using System.Net;
+
+namespace SocketServer
+{
+    class Program
+    {
+        /// <summary>
+        /// Socket Server 演示
+        /// 作者：陈希章
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //创建一个新的Socket,这里我们使用最常用的基于TCP的Stream Socket（流式套接字）
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //将该socket绑定到主机上面的某个端口
+            //方法参考：http://msdn.microsoft.com/zh-cn/library/system.net.sockets.socket.bind.aspx
+            socket.Bind(new IPEndPoint(IPAddress.Any, 4530));
+
+            //启动监听，并且设置一个最大的队列长度
+            //方法参考：http://msdn.microsoft.com/zh-cn/library/system.net.sockets.socket.listen(v=VS.100).aspx
+            socket.Listen(4);
+
+            //开始接受客户端连接请求
+            //方法参考：http://msdn.microsoft.com/zh-cn/library/system.net.sockets.socket.beginaccept.aspx
+            socket.BeginAccept(new AsyncCallback((ar) =>
+            {
+                //这就是客户端的Socket实例，我们后续可以将其保存起来
+                var client = socket.EndAccept(ar);
+
+                //给客户端发送一个欢迎消息
+                client.Send(Encoding.Unicode.GetBytes("Hi there, I accept you request at " + DateTime.Now.ToString()));
+            }), null);
+
+
+            Console.WriteLine("Server is ready!");
+            Console.Read();
+        }
+    }
+}
