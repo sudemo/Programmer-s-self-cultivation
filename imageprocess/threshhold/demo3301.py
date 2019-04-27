a460c708b998fcf7bf9c6af17e023fdf490b664c
neo <zwzhang@colibri.com.cn>
2019-04-27 18:07:13 +0800

    update

---
+++ imageprocess/threshhold/demo3301.py
@@ -0,0 +1,35 @@
+'''
+@author: neo
+@file: demo3301.py
+@time: 2019/4/27 16:48
+'''
+
+
+def fun1(arg):
+    ad = "sd"
+    arg = eval(arg)
+
+    # print(a)
+    def fun2():
+        if arg is not str:
+            for i in range (arg):
+                s = i*i
+                s += s
+            return s
+        else:
+            print("a")
+    def fun3():
+        if arg is str:
+            ss =""
+            for i in arg:
+                ss += i
+
+            return ss
+        # else:
+            # print("error")
+
+
+    return fun3()
+
+if __name__ == "__main__":
+    print(fun1("ad"))
