bac3a030eabcbc63527f26831bc236621dd05c0d
neo <zwzhang@colibri.com.cn>
2019-05-03 15:55:59 +0800

    update

--- pytorchdemo/cropbat.py
+++ pytorchdemo/cropbat.py
@@ -8,7 +8,7 @@ import os
 
 # path = "D:/Gitrepository/github/Programmer's self-cultivation/pytorchdemo/pic/train/j1/"
 # 此处有bug，无法区分' 导致地址错误
-path = "d:/pic/j1"
+path = "d:/pic/j2"
 lis = os.listdir(path)
 for i in range(len(lis)):
     img_path = os.path.join(path,lis[i])
@@ -20,6 +20,6 @@ for i in range(len(lis)):
         cropprd = img[200:1610,1500:1900]
         cv2.imshow('d:/pic/1.jpg', cropprd)
         cv2.waitKey(100)
-        cv2.imwrite("d:/pic/" + str(i) + ".jpg",cropprd)
+        cv2.imwrite("d:/pic/j2" + str(i) + ".jpg",cropprd)
 # cv2.imwrite(path,cropprd)
 # cv2.waitKey()
