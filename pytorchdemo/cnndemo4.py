5b66baad42f2e742119a35973d340a194cabacf9
neo <zwzhang@colibri.com.cn>
2019-05-04 19:03:33 +0800

    fix bugs

--- pytorchdemo/cnndemo4.py
+++ pytorchdemo/cnndemo4.py
@@ -39,18 +39,25 @@ def test_fun():
     eval_acc = 0
     # for eopch in range(EPOCH):
     for data in (test_loader):
+
         b_x, b_y = data
         b_x, b_y = Variable(b_x), Variable(b_y)  # b_x 是 img b_y是标签
         out_put = mymodel(b_x)
+
         loss = loss_func(out_put, b_y)
         eval_loss += loss.data.item() * b_y.size(0)
-        _, pred = torch.max(out_put, 1)
-        print("pred",pred,b_y)
-        num_correct = (pred == b_y).sum()
-        eval_acc += num_correct.item()
-        # print(eval_acc)
-        print('Test Loss: {:.4f}, acc:{}/{} ({:.0f}%)'.format(
-        eval_loss / (len(test_data)),
-        eval_acc ,(len(test_data)), 100. * eval_acc / len(test_data)
-        ))
+
+    predict_y = torch.argmax(out_put, 1).data.numpy()
+    _, pred = torch.max(out_put, 1)
+    # print("pred",pred,b_y)
+    # print("single",pred/b_y)
+    num_correct = (pred == b_y).sum()
+    print('Accuracy in Test : ', (num_correct * 100 ,len(predict_y)))
+    # print("acc: ", num_correct/b_y)
+    eval_acc += num_correct.item()
+    print(eval_acc)
+    # print('Test Loss: {:.4f}, acc:{}/{} ({:.0f}%)'.format(
+    # eval_loss / (len(test_data)),
+    # eval_acc ,(len(test_data)), 100. * eval_acc / len(test_data)
+    # ))
 test_fun()
