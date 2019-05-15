aff8485724a93e0e3042b978b6ab029f75a8b5ab
neo <zwzhang@colibri.com.cn>
2019-05-15 19:45:25 +0800

    update 自动检测xml 某个键的值

---
+++ C#/solution/xmlread/xmlmodify/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace xmlmodify
+{
+    class Program
+    {
+        public void Create(string xmlPath)
+        {
+            XDocument xDoc = XDocument.Load(xmlPath);
+            XElement xElement = xDoc.Element("BookStore");
+            xElement.Add(new XElement("Test", new XAttribute("Name", "Zery")));
+            xDoc.Save(xmlPath);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
