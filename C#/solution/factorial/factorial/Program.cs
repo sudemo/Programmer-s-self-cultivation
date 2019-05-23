74720e299a101426718dedd1400ff67c9154d852
neo <zwzhang@colibri.com.cn>
2019-05-23 10:41:04 +0800

    update exception

--- C#/solution/factorial/factorial/Program.cs
+++ C#/solution/factorial/factorial/Program.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace factorial
 {
     class Program
     {
+        public static int inta { get; private set; }
+
         private static long Factorial(int n)
         {
-            if (n == 1)
+            if (n == 1||n == 0)
                 return 1;
             long ret;
             ret = Factorial(n - 1) * n;
@@ -20,8 +23,21 @@ namespace factorial
 
         static void Main(string[] args)
         {
-           Console.WriteLine( Factorial(12));
-            Console.ReadKey();
+            Console.WriteLine("请输入一个整数：");
+            try
+            {
+                var a = Console.ReadLine();
+
+                int inta = Int32.Parse(a);                                
+                Console.WriteLine("not int");
+                Console.WriteLine("{0} 的阶乘is {1}", inta, Factorial(inta));
+                Console.Read();
+            }
+            catch
+            {
+                Console.WriteLine("wrong input");
+                Console.Read();
+            }
         }
     }
 }
