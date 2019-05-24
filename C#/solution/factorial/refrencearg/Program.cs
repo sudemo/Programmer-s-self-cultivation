bd0a83a5ae000b291772d7f8b728520a959836e3
neo <zwzhang@colibri.com.cn>
2019-05-24 10:30:14 +0800

    关于方法的参数类型

--- C#/solution/factorial/refrencearg/Program.cs
+++ C#/solution/factorial/refrencearg/Program.cs
@@ -9,30 +9,44 @@ namespace refrencearg
     class difclass
     {
         public int value = 100;
+        public int va = 12;
         //参数类型：值类型
-        static void ModifyValue(int value)
+        public static void ModifyValue(int arg) //静态方法必须将参数传入，才能使用。无法直接使用类内的字段
         {
-            value += value;
+            arg *= arg;
+            Console.WriteLine("arg is {0}",arg);
+           // value = 2*arg;
         }
         //引用类型，重载函数，参数类型不一致
         static void ModifyValue(difclass obj)
         {
             obj.value += 2;
         }
+
+        public void Addm() //可以直接使用类内的字段，因为他是非静态方法
+        {
+            
+            va = va + 1;
+        }
+        public void dispva()
+        {
+            Console.WriteLine("va is {0},value2 is {1}",va,value);
+        }
         static void Main(string[] args)
         {
             difclass obj = new difclass();
-            Console.WriteLine(obj.value);
+            Console.WriteLine("value is {0}",obj.value);
             //自动选择了适合参数类型的重载函数
             ModifyValue(obj.value);
+            Console.WriteLine("value2 is {0}", obj.value);
             ModifyValue(obj);
             Console.WriteLine(obj.value);
+            obj.Addm();
+            obj.dispva();
+            //Console.WriteLine();
             Console.ReadKey();
         }
 
-        //private static void ModifyValue(difclass difclass, object obj2)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        
     }
 }
