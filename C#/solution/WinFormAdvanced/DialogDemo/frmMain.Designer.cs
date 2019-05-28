04eddf1a69fe4481350120677d0744445d790236
neo <zwzhang@colibri.com.cn>
2019-05-28 17:25:06 +0800

    update winform

---
+++ C#/solution/WinFormAdvanced/DialogDemo/frmMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DialogDemo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.btnFontDialog = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.printForm1 = new Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(this.components);
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(15, 134);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(47, 12);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "lblInfo";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(17, 18);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(190, 37);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "使用OpenFileDialog打开文件";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(240, 16);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(190, 40);
+            this.btnSaveFile.TabIndex = 2;
+            this.btnSaveFile.Text = "使用SaveFileDialog保存文件";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // btnFontDialog
+            // 
+            this.btnFontDialog.Location = new System.Drawing.Point(17, 73);
+            this.btnFontDialog.Name = "btnFontDialog";
+            this.btnFontDialog.Size = new System.Drawing.Size(187, 42);
+            this.btnFontDialog.TabIndex = 3;
+            this.btnFontDialog.Text = "使用FontDialog定义字体";
+            this.btnFontDialog.UseVisualStyleBackColor = true;
+            this.btnFontDialog.Click += new System.EventHandler(this.btnFontDialog_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(240, 73);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(187, 42);
+            this.btnColor.TabIndex = 3;
+            this.btnColor.Text = "使用ColorDialog定义颜色";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // printForm1
+            // 
+            this.printForm1.DocumentName = "document";
+            this.printForm1.Form = this;
+            this.printForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter;
+            this.printForm1.PrinterSettings = ((System.Drawing.Printing.PrinterSettings)(resources.GetObject("printForm1.PrinterSettings")));
+            this.printForm1.PrintFileName = null;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 214);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnFontDialog);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.lblInfo);
+            this.Name = "frmMain";
+            this.Text = "对话框组件的使用";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button btnFontDialog;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnColor;
+        private Microsoft.VisualBasic.PowerPacks.Printing.PrintForm printForm1;
+    }
+}
+
---
+++ C#/solution/WinFormAdvanced/DialogDemo/frmMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogDemo
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "选择一张图片";
+            openFileDialog1.Filter = "所有支持的图片文件|*.jpg;*.gif;*.png;*.bmp|任意文件（*.*）|*.*";
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            //openFileDialog1.Multiselect = false;
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog1.Multiselect == false)
+                {
+                    lblInfo.Text = openFileDialog1.FileName;
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var file in openFileDialog1.FileNames)
+                    {
+                        sb.Append(file);
+                        sb.Append("\n");
+                    }
+                    lblInfo.Text = sb.ToString();
+                }
+
+
+
+            }
+
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.DefaultExt = ".txt";
+            saveFileDialog1.OverwritePrompt = true;
+            saveFileDialog1.Title = "保存文件";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblInfo.Text = "文件己保存到：" + saveFileDialog1.FileName;
+            }
+        }
+
+        private void btnFontDialog_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblInfo.Font = fontDialog1.Font;
+            }
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblInfo.ForeColor = colorDialog1.Color;
+            }
+        }
+    }
+}
