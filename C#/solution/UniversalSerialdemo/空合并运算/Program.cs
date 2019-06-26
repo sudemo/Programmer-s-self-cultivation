2104896118be9c80a48ca35dd482990dbcc525e9
neo <zwzhang@colibri.com.cn>
2019-06-26 17:50:20 +0800

    空合并运算

---
+++ C#/solution/UniversalSerialdemo/空合并运算/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 空合并运算
+{
+   
+    class NullCoalesce
+    {
+        static int? GetNullableInt()
+        {
+            return null;
+        }
+
+        static string GetStringValue()
+        {
+            return null;
+        }
+
+        static void Main()
+        {
+            int? x = null;
+            int y = x ?? -1;
+            Console.WriteLine(string.Format("x={0},y={1}", x, y));
+
+            int i = GetNullableInt() ?? default(int);
+            Console.WriteLine(string.Format("i={0} ", i));
+
+            string s = GetStringValue();
+            Console.WriteLine(s ?? "Unspecified");
+
+            Console.ReadKey();
+        }
+    }
+}
