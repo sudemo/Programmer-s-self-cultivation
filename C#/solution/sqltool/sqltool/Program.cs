0423c6d05a6c8f6e28f6602c0cf22c804ef13e98
neo <zwzhang@colibri.com.cn>
2019-06-27 17:48:01 +0800

    写数据

--- C#/solution/sqltool/sqltool/Program.cs
+++ C#/solution/sqltool/sqltool/Program.cs
@@ -17,6 +17,16 @@ namespace sqltool
             //打开数据库，若文件不存在会自动创建 
             con.Open();
             Console.WriteLine("ok");
+            
+        }
+        public void writesql()
+        {
+            string dbPath = "Data Source =" + Environment.CurrentDirectory + "/test.db";
+            SQLiteConnection con = new SQLiteConnection(dbPath);
+            con.Open();
+            string sql = "CREATE TABLE IF NOT EXISTS student(id integer, name varchar(20), sex varchar(2));";
+            SQLiteCommand com = new SQLiteCommand(sql, con);
+            com.ExecuteNonQuery();
         }
     }
     class Program
@@ -24,13 +34,15 @@ namespace sqltool
        
         static void Main(string[] args)
         {
-            string path = "Data Source="+ System.Environment.CurrentDirectory + "\\test.db3"+";Version=3;";
-            
-            CSqliteWrapper._S_Init(path);
+            //string path = "Data Source="+ System.Environment.CurrentDirectory + "\\test.db3"+";Version=3;";
+
+            //CSqliteWrapper._S_Init(path);
             //CSqliteWrapper._S_ExecuteSql("hello");
 
-            //tets a = new tets();
+            tets a = new tets();
             //a.testdata();
+            a.writesql();
+            
         }
     }
 }
