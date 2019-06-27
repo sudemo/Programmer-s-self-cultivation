3fbca539847d54ce2890c9678372fb09457fc1bf
neo <zwzhang@colibri.com.cn>
2019-06-27 17:37:13 +0800

    创建一个sqllite 的数据库

---
+++ C#/solution/sqltool/sqltool/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nsCommoner;
+using System.Data.SQLite;
+namespace sqltool
+{
+    class tets
+    {
+        public void testdata()
+        {
+            string dbPath = "Data Source =" + Environment.CurrentDirectory + "/test.db";
+            //创建连接数据库实例，指定文件位置 
+            SQLiteConnection con = new SQLiteConnection(dbPath);
+            //打开数据库，若文件不存在会自动创建 
+            con.Open();
+            Console.WriteLine("ok");
+        }
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string path = "Data Source="+ System.Environment.CurrentDirectory + "\\test.db3"+";Version=3;";
+            
+            CSqliteWrapper._S_Init(path);
+            //CSqliteWrapper._S_ExecuteSql("hello");
+
+            //tets a = new tets();
+            //a.testdata();
+        }
+    }
+}
---
+++ C#/solution/sqltool/sqltool/sqltool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using MySql.Data.MySqlClient;
+//using MySql.Data.MySqlClient;
+using System.Data.SQLite;
+namespace sqltool
+{
+    class CreateDatabase
+    {
+//        public void configdatabase()
+//        {
+//            string configstring = "Data Source=localhost;Persist Security Info=yes;UserId=root; PWD=123456";
+//            //MySqlConnectionStringBuilder tdatabase = new MySqlConnectionStringBuilder();
+//            MySqlConnection aconnect = new MySqlConnection(configstring);
+//            //MySqlConnection m_conn = new MySqlConnection("server=localhost;user id=root;password=root;persist security info=True");
+//            MySqlCommand cmd = new MySqlCommand("CREATE DATABASE yiersan;", aconnect);
+//            aconnect.Open();
+//            Console.WriteLine("as");
+////return 1;
+//        }
+
+
+    }
+}
