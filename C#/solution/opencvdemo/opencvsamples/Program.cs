e518756e87544d1046222d8823aa1e5cd6a060ff
neo <zwzhang@colibri.com.cn>
2019-07-01 10:28:46 +0800

    update opencvsamples

---
+++ C#/solution/opencvdemo/opencvsamples/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace opencvsamples
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ISample sample =
+                //new BgSubtractorMOG();
+                //new BinarizerSample();
+                //new BRISKSample();
+                //new CaffeSample();
+                //new ClaheSample();
+                //new ConnectedComponentsSample();
+                //new DFT();
+                //new FaceDetection();
+                //new FASTSample();
+                //new FlannSample(); 
+                //new FREAKSample();
+                new HistSample();
+            //new HOGSample();
+            //new HoughLinesSample();
+            //new KAZESample2();
+            //new KAZESample();
+            //new MatOperations();
+            //new MatToBitmap();
+            //new MDS();
+            //new MSERSample();
+            //new NormalArrayOperations();
+            //new PhotoMethods();
+            //new MergeSplitSample();
+            //new MorphologySample();
+            //new PixelAccess();
+            //new SeamlessClone();
+            //new SiftSurfSample();
+            //new SolveEquation();
+            //new StarDetectorSample();
+            //new Stitching();
+            //new Subdiv2DSample();
+            //new SuperResolutionSample();
+            //new SVMSample();
+            //new VideoWriterSample();
+            //new VideoCaptureSample();
+
+            sample.Run();
+        }
+    }
+}
