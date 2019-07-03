4585b2f9b0d52e40eb189815f8f4476f33a44fd7
neo <zwzhang@colibri.com.cn>
2019-07-03 10:12:22 +0800

    update camera

--- C#/practice/ClassLibrary1/testdll/Program.cs
+++ C#/practice/ClassLibrary1/testdll/Program.cs
@@ -10,25 +10,37 @@ namespace testdll
     {
         static void Main()
         {
-            Mat src1 = Cv2.ImRead("d:/2.jpg", ImreadModes.Grayscale);
+            Mat src1 = Cv2.ImRead("d:/lena1.jpg", ImreadModes.Grayscale);
             Mat dst = new Mat();
             /* 
-data:  需要自动聚类的数据，一般是一个Mat。浮点型的矩阵，每行为一个样本。
-k: 取成几类，比较关键的一个参数。
-bestLabels:  返回的类别标记,整型数字。
-criteria: 算法结束的标准，获取期望精度的迭代最大次数
-attempts:  判断某个样本为某个类的最少聚类次数，比如值为3时，则某个样本聚类3次都为同一个类，则确定下来。
-flags:  确定簇心的计算方式。有三个值可选：KMEANS_RANDOM_CENTERS 表示随机初始化簇心。KMEANS_PP_CENTERS 表示用kmeans++算法来初始化簇心（没用过），KMEANS_USE_INITIAL_LABELS 表示第一次聚类时用用户给定的值初始化聚类，后面几次的聚类，则自动确定簇心。
-centers: 用来初始化簇心的。与前一个flags参数的选择有关。如果选择KMEANS_RANDOM_CENTERS随机初始化簇心，则这个参数可省略。
+            data:  需要自动聚类的数据，一般是一个Mat。浮点型的矩阵，每行为一个样本。
+            k: 取成几类，比较关键的一个参数。
+            bestLabels:  返回的类别标记,整型数字。
+            criteria: 算法结束的标准，获取期望精度的迭代最大次数
+            attempts:  判断某个样本为某个类的最少聚类次数，比如值为3时，则某个样本聚类3次都为同一个类，则确定下来。
+            flags:  确定簇心的计算方式。有三个值可选：KMEANS_RANDOM_CENTERS 表示随机初始化簇心。KMEANS_PP_CENTERS 表示用kmeans++算法来初始化簇心（没用过），KMEANS_USE_INITIAL_LABELS 表示第一次聚类时用用户给定的值初始化聚类，后面几次的聚类，则自动确定簇心。
+            centers: 用来初始化簇心的。与前一个flags参数的选择有关。如果选择KMEANS_RANDOM_CENTERS随机初始化簇心，则这个参数可省略。
              */
             //Mat getLabels() { return labels; };
-           
-            Mat samples = new Mat(new IntPtr (MatType.CV_64F));
-            Cv2.Kmeans(src1, k: 2,bestLabels:samples, criteria:new TermCriteria(CriteriaType.MaxIter, 10, 0.1), attempts: 2, flags:KMeansFlags.RandomCenters);
+
+            //Mat samples = new Mat(new IntPtr (MatType.CV_64F));
+            //Cv2.Kmeans(src1, k: 2,bestLabels:samples, criteria:new TermCriteria(CriteriaType.MaxIter, 10, 0.1), attempts: 2, flags:KMeansFlags.RandomCenters);
             //Cv2.Canny(src1, dst, 30, 30);
-            //using (new Window("src image", src))
-           Cv2.ImShow("dst image", dst);           
-                Cv2.WaitKey();
+            //using (new Window("src image", src1))
+            //Cv2.ImShow("dst image", dst);
+            Cv2.PutText(src1,                                      //目标图像
+                       "baymax",                        //字符串
+                       new Point(0, 80),                //位置，注意这是字符串左下角的位置
+                       HersheyFonts.HersheySimplex,  //字体类型
+                       5,                              //字体大小   
+                       Scalar.Green);
+        
+           
+            Rect roi = new Rect(100,50,300,300);
+            Cv2.Rectangle(src1,roi,Scalar.AliceBlue);
+            //Mat irot = new Mat(src1, roi);
+            Cv2.ImShow("roi",src1);
+            Cv2.WaitKey();
             
         }
     }
---
+++ C#/practice/camera/camera/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using System.IO;
+
+namespace camera
+{
+    class CaptureService
+    {
+        public static void DoSomeThing(DateTime? startTime, long count)
+        {
+
+            var cap = VideoCapture.FromCamera(CaptureDevice.Any);
+            cap.Set(CaptureProperty.FrameWidth, 512);
+            cap.Set(CaptureProperty.FrameHeight, 300);
+            Mat mat = new Mat();
+            //while ())
+            {
+                cap.Read(mat);
+                File.WriteAllBytes(DateTime.Now.Ticks + ".png", mat.ToBytes());
+
+            }
+            cap.Release();
+            cap.Dispose();
+        }
+        public static void Main()
+        {
+            DoSomeThing(new DateTime() ,1);
+        }
+    }
+}
