cfd3a374babde11848e801adc1c02905a615ad20
neo <zwzhang@colibri.com.cn>
2019-07-04 14:36:34 +0800

    testdll

--- C#/practice/ClassLibrary1/testdll/Program.cs
+++ C#/practice/ClassLibrary1/testdll/Program.cs
@@ -10,7 +10,7 @@ namespace testdll
     {
         static void Main()
         {
-            Mat src1 = Cv2.ImRead("d:/lena1.jpg", ImreadModes.Grayscale);
+            Mat src1 = Cv2.ImRead("d:/lena1.jpg", ImreadModes.AnyColor);
             Mat dst = new Mat();
             /* 
             data:  需要自动聚类的数据，一般是一个Mat。浮点型的矩阵，每行为一个样本。
@@ -22,12 +22,22 @@ namespace testdll
             centers: 用来初始化簇心的。与前一个flags参数的选择有关。如果选择KMEANS_RANDOM_CENTERS随机初始化簇心，则这个参数可省略。
              */
             //Mat getLabels() { return labels; };
+            var columnVect = src1.Reshape(cn:0);
 
-            //Mat samples = new Mat(new IntPtr (MatType.CV_64F));
-            //Cv2.Kmeans(src1, k: 2,bestLabels:samples, criteria:new TermCriteria(CriteriaType.MaxIter, 10, 0.1), attempts: 2, flags:KMeansFlags.RandomCenters);
+
+            Mat samples = new Mat(new IntPtr(MatType.CV_64F));
+            columnVect.ConvertTo(samples,MatType.CV_32FC3);
+            var bestLabels = new Mat();
+            var centers = new Mat();
+            //var centers = new Mat(rows: clustersCount, cols: 1, type: points.Type());
+            Cv2.Kmeans(samples, k: 2, bestLabels: samples, criteria: new TermCriteria(CriteriaType.MaxIter, 10, 0.1), attempts: 2, flags: KMeansFlags.RandomCenters,centers:centers);
             //Cv2.Canny(src1, dst, 30, 30);
-            //using (new Window("src image", src1))
-            //Cv2.ImShow("dst image", dst);
+            //var contours;
+            //HierarchyIndex[] _;
+            //Cv2.FindContours(src1, out contours, out _, RetrievalModes.External,
+            //               ContourApproximationModes.ApproxNone);
+            using (new Window("src image", src1))
+               // Cv2.ImShow("dst image", dst);
             Cv2.PutText(src1,                                      //目标图像
                        "baymax",                        //字符串
                        new Point(0, 80),                //位置，注意这是字符串左下角的位置
