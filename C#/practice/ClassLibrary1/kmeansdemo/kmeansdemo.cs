088379976da7d8832768f8e6e661647c85b59f48
neo <zwzhang@colibri.com.cn>
2019-07-04 14:37:09 +0800

    kmeans demo

---
+++ C#/practice/ClassLibrary1/kmeansdemo/kmeansdemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kmeansdemo
+{
+    static class kmeansdemo
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            kmeans k = new kmeans();
+            k.qoute();
+        }
+    }
+}
