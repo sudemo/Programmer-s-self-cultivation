48c24acc5b8143898ba023c86f30094344f93821
neo <zwzhang@colibri.com.cn>
2019-07-05 15:50:04 +0800

    修改 listbox2 的功能，仅显示，无响应事件

--- C#/practice/ClassLibrary1/winfopencv/FormMain.Designer.cs
+++ C#/practice/ClassLibrary1/winfopencv/FormMain.Designer.cs
@@ -45,15 +45,14 @@
             this.BtnStart.TabIndex = 0;
             this.BtnStart.Text = "BtnStart";
             this.BtnStart.UseVisualStyleBackColor = true;
-            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
             // 
             // BtnRun
             // 
             this.BtnRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.BtnRun.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.BtnRun.Location = new System.Drawing.Point(345, 309);
+            this.BtnRun.Location = new System.Drawing.Point(325, 293);
             this.BtnRun.Name = "BtnRun";
-            this.BtnRun.Size = new System.Drawing.Size(75, 23);
+            this.BtnRun.Size = new System.Drawing.Size(107, 54);
             this.BtnRun.TabIndex = 1;
             this.BtnRun.Text = "BtnRun";
             this.BtnRun.UseVisualStyleBackColor = true;
@@ -61,7 +60,6 @@
             // 
             // listBox1
             // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
             this.listBox1.Items.AddRange(new object[] {
@@ -94,7 +92,7 @@
             "人脸识别"});
             this.listBox1.Location = new System.Drawing.Point(0, 0);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 338);
+            this.listBox1.Size = new System.Drawing.Size(107, 352);
             this.listBox1.TabIndex = 6;
             this.listBox1.TabStop = false;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
@@ -104,24 +102,18 @@
             this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 12;
-            this.listBox2.Items.AddRange(new object[] {
-            "1",
-            "12",
-            "2",
-            "3"});
             this.listBox2.Location = new System.Drawing.Point(325, 0);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(107, 268);
             this.listBox2.TabIndex = 7;
             this.listBox2.TabStop = false;
-            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.textBox1.Location = new System.Drawing.Point(0, 338);
+            this.textBox1.Location = new System.Drawing.Point(108, 293);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(432, 21);
+            this.textBox1.Size = new System.Drawing.Size(211, 59);
             this.textBox1.TabIndex = 8;
             // 
             // pictureBox1
@@ -129,9 +121,10 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(113, 0);
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pictureBox1.Location = new System.Drawing.Point(108, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(211, 338);
+            this.pictureBox1.Size = new System.Drawing.Size(211, 268);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
--- C#/practice/ClassLibrary1/winfopencv/FormMain.cs
+++ C#/practice/ClassLibrary1/winfopencv/FormMain.cs
@@ -11,8 +11,6 @@ namespace winfopencv
     public partial class FormMain : Form
     {
         
-        private PictureBoxIpl _pictureBoxIpl1;
-        private BackgroundWorker _worker;
         
         public FormMain()
         {
@@ -61,135 +59,25 @@ namespace winfopencv
             }
         }
 
-        
-       
-        private void BtnStart_Click(object sender, System.EventArgs e)
-        {
-            if (_worker != null && _worker.IsBusy)
-            {
-                return;
-            }
-
-            _worker = new BackgroundWorker
-            {
-                WorkerReportsProgress = true,
-                WorkerSupportsCancellation = true
-            };
-
-            if (RadioAvi.Checked)
-            {
-                _worker.DoWork += workerDoReadVideo;
-            }
-
-            if (RadioWebCam.Checked)
-            {
-                _worker.DoWork += workerDoReadCamera;
-            }
-
-            _worker.ProgressChanged += workerProgressChanged;
-            _worker.RunWorkerCompleted += workerRunWorkerCompleted;
-            _worker.RunWorkerAsync();
-
-            BtnStart.Enabled = false;
-        }
-
-        private void BtnRun_Click(object sender, System.EventArgs e)
-        {
-            obj_opencv_method.opencv_method_run(input_img: a, output_img: b);
-
-            //if (_worker != null)
-            //{
-            //    _worker.CancelAsync();
-            //    _worker.Dispose();
-            //}
-            //BtnStart.Enabled = true;
-
-        }
-
-        private void FrmMain_Load(object sender, System.EventArgs e)
-        {
-            _pictureBoxIpl1 = new PictureBoxIpl
-            {
-                AutoSize = true
-            };
-            //flowLayoutPanel1.Controls.Add(_pictureBoxIpl1);
-        }
-        #region 其他程序
-        private void workerDoReadCamera(object sender, DoWorkEventArgs e)
-        {
-            using (var capture = new VideoCapture(CaptureDevice.Any, index: 0))
-            {
-                var fps = getFps(capture);
-                capture.Fps = fps;
-                var interval = (int)(1000 / fps);
-
-                using (var image = new Mat())
-                {
-                    while (_worker != null && !_worker.CancellationPending)
-                    {
-                        capture.Read(image);
-                        if (image.Empty())
-                            break;
-
-                        _worker.ReportProgress(0, image);
-                        Thread.Sleep(interval);
-                    }
-                }
-            }
-        }
-        private void workerDoReadVideo(object sender, DoWorkEventArgs e)
-        {
-            using (var capture = new VideoCapture(@"C:/Users/zwzhang/Pictures/Movie/bach.mp4"))
-            {
-                var interval = (int)(1000 / capture.Fps);
-
-                using (var image = new Mat())
-                {
-                    while (_worker != null && !_worker.CancellationPending)
-                    {
-                        capture.Read(image);
-                        if (image.Empty())
-                        {
-                            break;
-                        }
-
-                        _worker.ReportProgress(0, image);
-                        Thread.Sleep(interval);
-                    }
-                }
-            }
-        }
-
-        private void workerProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-
-            var image = e.UserState as Mat;
-            if (image == null) return;
-
-            //Cv.Not(image, image);
-            _pictureBoxIpl1.RefreshIplImage(image);
-        }
-
-        private void workerRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            _worker.Dispose();
-            _worker = null;
-            BtnStart.Enabled = true;
-        } 
-        #endregion
-
         private void listBox1_SelectedIndexChanged(object sender, System.EventArgs e)
         {
             int methodnum; 
-            methodnum = listBox1.SelectedIndex;           
-            
+            methodnum = listBox1.SelectedIndex;
+            listBox2.Items.Add(listBox1.SelectedItem);
+            textBox1.AppendText("成功添加" + listBox1.SelectedItem.ToString() + "!");
+
         }
 
         private void listBox2_SelectedIndexChanged(object sender, System.EventArgs e)
         {
-            MessageBox.Show("how r u");
+            
         }
 
-       
+        private void BtnRun_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("ok");
+        }
+
+        
     }
 }
