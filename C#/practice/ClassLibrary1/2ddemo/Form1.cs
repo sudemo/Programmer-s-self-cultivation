f98848a60195161337eac68a6603078aca64aa94
neo <zwzhang@colibri.com.cn>
2019-07-15 14:47:26 +0800

    qr code demo

--- C#/practice/ClassLibrary1/2ddemo/Form1.cs
+++ C#/practice/ClassLibrary1/2ddemo/Form1.cs
@@ -1,40 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
-using ZXing;
 
 
 namespace barcodedemo
 {
     public partial class Form1 : Form
-        {
-
+    {
+        private VideoCapture capture;
+        bool Capturing;
+        private readonly IBarcodeReaderImage reader;
         public Form1()
-    {
-        InitializeComponent();
-        reader = new BarcodeReaderImage();
-    }
-
-    private void btnOpenFile_Click(object sender, EventArgs e)
-    {
-        using (var openFileDialog = new OpenFileDialog())
         {
-            openFileDialog.Multiselect = false;
-            openFileDialog.Filter = "TIFF (*.tif,*.tiff)|*.tif;*.tiff| JPG (*.jpg,*.jpeg)|*.jpg *.jpeg|*.*|*.*";
-            try
+            InitializeComponent();
+            reader = new BarcodeReaderImage();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
             {
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                openFileDialog.Multiselect = false;
+                openFileDialog.Filter = "TIFF (*.tif,*.tiff)|*.tif;*.tiff| JPG (*.jpg,*.jpeg)|*.jpg *.jpeg|*.*|*.*";
+                try
                 {
-                    var src = new Mat(openFileDialog.FileName);
-                    var result = reader.Decode(src);
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        var src = new Mat(openFileDialog.FileName);
+                        var result = reader.Decode(src);
+                        if (result != null)
+                        {
+                            txtResult.Text = result.Text;
+                            txtResultBarcodeType.Text = result.BarcodeFormat.ToString();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                try
+                {
+                    capture = VideoCapture.FromCamera(0);
+                }
+                catch (NullReferenceException exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+                catch (OpenCvSharpException exc)
+                {
+                    MessageBox.Show(
+                       "Attention: You have to copy all the assemblies and native libraries from an official release of OpenCV to the directory of the demo." +
+                       Environment.NewLine + Environment.NewLine + exc);
+                }
+            }
+
+            if (capture != null)
+            {
+                if (Capturing)
+                {
+                    captureButton.Text = "Start Capturing";
+                    Application.Idle -= DoDecoding;
+                }
+                else
+                {
+                    captureButton.Text = "Stop Capturing";
+                    Application.Idle += DoDecoding;
+                }
+                Capturing = !Capturing;
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if (capture != null)
+                capture.Dispose();
+        }
+
+
+        private void DoDecoding(object sender, EventArgs args)
+        {
+            var timerStart = DateTime.Now.Ticks;
+
+            var image = capture.RetrieveMat();
+            if (image != null &&
+                image.Height > 0)
+            {
+                using (image)
+                {
+                    // show it
+                    pictureBox1.Image = image.ToBitmap();
+                    // decode it
+                    var result = reader.Decode(image);
+                    // show result
                     if (result != null)
                     {
                         txtResult.Text = result.Text;
@@ -42,85 +108,13 @@ namespace barcodedemo
                     }
                 }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            var timerStop = DateTime.Now.Ticks;
+            labDuration.Text = new TimeSpan(timerStop - timerStart).Milliseconds.ToString("0 ms");
         }
-    }
 
-    private void captureButton_Click(object sender, EventArgs e)
-    {
-        if (capture == null)
+        private void btnClose_Click(object sender, EventArgs e)
         {
-            try
-            {
-                capture = VideoCapture.FromCamera(0);
-            }
-            catch (NullReferenceException exception)
-            {
-                MessageBox.Show(exception.Message);
-            }
-            catch (OpenCvSharpException exc)
-            {
-                MessageBox.Show(
-                   "Attention: You have to copy all the assemblies and native libraries from an official release of OpenCV to the directory of the demo." +
-                   Environment.NewLine + Environment.NewLine + exc);
-            }
-        }
-
-        if (capture != null)
-        {
-            if (Capturing)
-            {
-                captureButton.Text = "Start Capturing";
-                Application.Idle -= DoDecoding;
-            }
-            else
-            {
-                captureButton.Text = "Stop Capturing";
-                Application.Idle += DoDecoding;
-            }
-            Capturing = !Capturing;
+            this.Close();
         }
     }
-
-    protected override void OnClosed(EventArgs e)
-    {
-        if (capture != null)
-            capture.Dispose();
-    }
-
-
-    private void DoDecoding(object sender, EventArgs args)
-    {
-        var timerStart = DateTime.Now.Ticks;
-
-        var image = capture.RetrieveMat();
-        if (image != null &&
-            image.Height > 0)
-        {
-            using (image)
-            {
-                // show it
-                pictureBox1.Image = image.ToBitmap();
-                // decode it
-                var result = reader.Decode(image);
-                // show result
-                if (result != null)
-                {
-                    txtResult.Text = result.Text;
-                    txtResultBarcodeType.Text = result.BarcodeFormat.ToString();
-                }
-            }
-        }
-        var timerStop = DateTime.Now.Ticks;
-        labDuration.Text = new TimeSpan(timerStop - timerStart).Milliseconds.ToString("0 ms");
-    }
-
-    private void btnClose_Click(object sender, EventArgs e)
-    {
-        this.Close();
-    }
-}
 }
