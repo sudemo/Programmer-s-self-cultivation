f5d8bf8643bdde0239d98931febce729ab2127c7
neo <zwzhang@colibri.com.cn>
2019-07-15 14:06:15 +0800

    barcode demo

---
+++ C#/practice/ClassLibrary1/2ddemo/BarcodeReaderImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using OpenCvSharp;
+
+using ZXing;
+
+namespace _2dbarcode
+{
+    /// <summary>
+    /// a barcode reader class which can be used with the Mat type from OpenCVSharp
+    /// </summary>
+    public class BarcodeReaderImage : BarcodeReader<Mat>, IBarcodeReaderImage
+    {
+        /// <summary>
+        /// define a custom function for creation of a luminance source with our specialized Mat-supporting class
+        /// </summary>
+        private static readonly Func<Mat, LuminanceSource> defaultCreateLuminanceSource =
+           (image) => new ImageLuminanceSource(image);
+
+        /// <summary>
+        /// constructor which uses a custom luminance source with Mat support
+        /// </summary>
+        public BarcodeReaderImage()
+           : base(null, defaultCreateLuminanceSource, null)
+        {
+        }
+    }
+}
---
+++ C#/practice/ClassLibrary1/2ddemo/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing;
+using OpenCvSharp;
+
+namespace _2ddemo
+{
+    /// <summary>
+    /// interface for a barcode reader class which can be used with the Mat type from OpenCVSharp
+    /// </summary>
+    public interface IBarcodeReaderImage : IBarcodeReader<Mat>
+    {
+    }
+}
