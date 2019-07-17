65d152541d324e30ac9f6d1f32f0f1dddc578254
neo <zwzhang@colibri.com.cn>
2019-07-17 14:42:29 +0800

    1442

---
+++ C#/practice/PLCCommunication/PLCCommunication/LSIS/LSCpuInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HslCommunication.Profinet.LSIS
+{
+    /// <summary>
+    /// It is determined to be the XGK/I/R series through a reserved area
+    /// </summary>
+    public enum LSCpuInfo
+    {
+        XGK = 1,
+        XGI,
+        XGR,
+        XGB_MK,
+        XGB_IEC,
+    }
+
+    public enum LSCpuStatus
+    {
+        RUN = 1,
+        STOP,
+        ERROR,
+        DEBUG
+    }
+    /// <summary>
+    /// using FlagBit in Marker for Byte
+    /// M0.0=1;M0.1=2;M0.2=4;M0.3=8;==========================>M0.7=128
+    /// </summary>
+    public enum FlagBit
+    {
+        Flag1 = 1,
+        Flag2 = 2,
+        Flag4 = 4,
+        Flag8 = 8,
+        Flag16 = 16,
+        Flag32 = 32,
+        Flag64 = 64,
+        Flag128 = 128,
+        Flag256 = 256,
+
+    }
+}
---
+++ C#/practice/PLCCommunication/PLCCommunication/SocketBase/status.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PLCCommunicationLibrary.Language;
+
+namespace PLCCommunicationLibray.SocketBase
+{
+    class status
+    {
+        /// <summary>
+        /// 指示本次访问是否成功
+        /// </summary>
+        public bool IsSuccess { get; set; }
+
+        /// <summary>
+        /// 具体的错误描述
+        /// </summary>
+        public string Message { get; set; } = StringResources.LanguageSet.UnknownError;
+
+        /// <summary>
+        /// 具体的错误代码
+        /// </summary>
+        public int ErrorCode { get; set; } = 10000;
+
+        /// <summary>
+        /// 获取错误代号及文本描述
+        /// </summary>
+        /// <returns>包含错误码及错误消息</returns>
+        public string ToMessageShowString()
+        {
+           return $"{StringResources.LanguageSet.ErrorCode}:{ErrorCode}{Environment.NewLine}{StringResources.LanguageSet.TextDescription}:{Message}";
+        }
+    }
+
+    public static class StringResources
+    {
+        #region Constractor
+
+        static StringResources()
+        {
+            if (System.Globalization.CultureInfo.CurrentCulture.ToString().StartsWith("zh"))
+            {
+                SetLanguageChinese();
+            }
+            else
+            {
+                SeteLanguageEnglish();
+            }
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// 获取或设置系统的语言选项 ->
+        /// Gets or sets the language options for the system
+        /// </summary>
+        public static DefaultLanguage LanguageSet = new DefaultLanguage();
+
+        /// <summary>
+        /// 将语言设置为中文 ->
+        /// Set the language to Chinese
+        /// </summary>
+        public static void SetLanguageChinese()
+        {
+            LanguageSet = new DefaultLanguage();
+        }
+
+        /// <summary>
+        /// 将语言设置为英文 ->
+        /// Set the language to English
+        /// </summary>
+        public static void SeteLanguageEnglish()
+        {
+            LanguageSet = new English();
+        }
+    }
+}
