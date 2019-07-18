cb5b0642dddd058802cb3df4d85ed50c2198ed79
neo <zwzhang@colibri.com.cn>
2019-07-18 16:19:08 +0800

    update status method

--- C#/practice/PLCCommunication/PLCCommunication/SocketBase/SocketBase.cs
+++ C#/practice/PLCCommunication/PLCCommunication/SocketBase/SocketBase.cs
@@ -6,9 +6,35 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 
+
 namespace PLCCommunicationLibrary.SocketBase
 {
     class SocketBase
     {
+        public Socket PLCClient; //字段
+        private bool ConnectionStatus;
+
+        
+        public ReturnStatus<Socket> CreatandConnect(IPEndPoint endpoint,int timeout)//创建并连接socket
+        {
+            
+            PLCClient = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            try
+               
+            {   
+                timeout = 100;
+                PLCClient.Connect(endpoint);
+                //ReturnStatus<Socket> ReturnStatus = new ReturnStatus();
+                //ReturnStatus.CreateSuccessResult
+              
+                return ReturnStatus.CreatSuccessStatus(PLCClient);
+            }
+            catch (Exception e)
+            {
+                PLCClient?.Close();
+                //log
+                return new ReturnStatus<Socket>(e.Message);
+            }           
+        }
     }
 }
