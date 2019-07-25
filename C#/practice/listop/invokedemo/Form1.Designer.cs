60973e65c9f70693c30273426d4b58c77c17cac9
neo <zwzhang@colibri.com.cn>
2019-07-25 08:42:38 +0800

    update test demo

---
+++ C#/practice/listop/invokedemo/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace invokedemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.downloadButton.Location = new System.Drawing.Point(49, 55);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(99, 47);
+            this.downloadButton.TabIndex = 0;
+            this.downloadButton.Text = "button1";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.downloadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button downloadButton;
+    }
+}
---
+++ C#/practice/listop/invokedemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Threading.Tasks;
+using System.Threading;
+using System.Security.Policy;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+
+namespace invokedemo
+{
+    class Program
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        [HttpGet]
+        [Out("DotNetCount")]
+        public async Task<int> GetDotNetCountAsync()
+        {
+            // Suspends GetDotNetCountAsync() to allow the caller (the web server)
+            // to accept another request, rather than blocking on this one.
+            var html = await _httpClient.GetStringAsync("https://dotnetfoundation.org");
+
+            return Regex.Matches(html, @"\.NET").Count;
+        }
+
+        /*private void mathread()
+        {
+            MessageBox.Show(Thread.CurrentThread.GetHashCode().ToString() + "AAA");
+            var  invokeThread = new Thread(new ThreadStart(StartMethod));
+            invokeThread.Start();
+            string a = string.Empty;
+            for (int i = 0; i < 3; i++)      //调整循环次数，看的会更清楚
+            {
+                Thread.Sleep(1000);
+                a = a + "B";
+            }
+            MessageBox.Show(Thread.CurrentThread.GetHashCode().ToString() + a);
+        }
+
+        private void StartMethod()
+        {
+            MessageBox.Show(Thread.CurrentThread.GetHashCode().ToString() + "CCC");
+            button1.BeginInvoke(new invokeDelegate(invokeMethod));
+            MessageBox.Show(Thread.CurrentThread.GetHashCode().ToString() + "DDD");
+        }
+
+        private void beginInvokeMethod()
+        {
+            //Thread.Sleep(3000);
+            MessageBox.Show(Thread.CurrentThread.GetHashCode().ToString() + "EEEEEEEEEEEE");
+        }*/
+        static void Main(string[] args)
+    {
+    }
+}
+}
