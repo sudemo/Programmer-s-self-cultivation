9939b25a1e4265a480e97c4d1ece61d27336a4e9
zwzhang <zwzhang@colibri.com.cn>
2019-08-08 12:23:23 +0800

    json文件第一行没有空一格，导致解析出错

---
+++ C#/solution/jsondemo/jsondemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace jsondemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadJson("speed");
+        }
+        public static void ReadJson(string key)
+        {
+            //string jspath = "d://Configuration//Configuration.yaml";
+            string jspath = "d://Configuration//js.json";
+            StreamReader sr = new StreamReader(jspath);
+            int a=sr.Read();
+            Console.WriteLine(a);
+            //Newtonsoft.Json.JsonTextReader ss = new Newtonsoft.Json.JsonTextReader(sr);
+            JsonTextReader reader = new JsonTextReader(sr);
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.TokenType + "\t" + reader.ValueType + "\t" + reader.Value);
+            }
+            Console.Read();
+            //jt.Read();
+            //var value = jt.Value;
+            //return 
+        }
+    }
+
+}
