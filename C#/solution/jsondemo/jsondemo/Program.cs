8b953f32329cbb487e0773a8e37772ccc1a35665
zwzhang <zwzhang@colibri.com.cn>
2019-08-08 17:29:43 +0800

    一直提示未将对象实例化，仔细检查发现是json key name error

--- C#/solution/jsondemo/jsondemo/Program.cs
+++ C#/solution/jsondemo/jsondemo/Program.cs
@@ -11,37 +11,59 @@ namespace jsondemo
 {
     class Program
     {
+        private static readonly string jspath = "d://Configuration//js.json";
         static void Main(string[] args)
         {
-            // ReadJson("speed");
-            readjson();
-        }
-        public static void ReadJson(string key)
-        {
-            //string jspath = "d://Configuration//Configuration.yaml";
-            string jspath = "d://Configuration//js.json";
-            StreamReader sr = new StreamReader(jspath);
-            int a=sr.Read();
-            //Console.WriteLine(a);
-            //Newtonsoft.Json.JsonTextReader ss = new Newtonsoft.Json.JsonTextReader(sr);
-            JsonTextReader reader = new JsonTextReader(sr);
-            reader.Read();
-            var s = reader.Value;
-            //while (reader.Read())
-            //{
-            //    Console.WriteLine(reader.TokenType + "\t" + reader.ValueType + "\t" + reader.Value);
-            //}
-            Console.WriteLine(reader.Value);
-            Console.Read();
-            //jt.Read();
-            //var value = jt.Value;
-            //return 
-        }
-
-        public static void readjson()
-        {
-            string jspath = "d://Configuration//js.json";
+            demo1.ReadJson1();
             
+            //sReadJson();
+            //readjson();
+        }
+        //public static void ReadJson(string key)
+        //{
+        //    //string jspath = "d://Configuration//Configuration.yaml";
+        //    string jspath = "d://Configuration//js.json";
+        //    StreamReader sr = new StreamReader(jspath);
+        //    int a=sr.Read();
+        //    //Console.WriteLine(a);
+        //    //Newtonsoft.Json.JsonTextReader ss = new Newtonsoft.Json.JsonTextReader(sr);
+        //    JsonTextReader reader = new JsonTextReader(sr);
+        //    reader.Read();
+        //    var s = reader.Value;
+        //    //while (reader.Read())
+        //    //{
+        //    //    Console.WriteLine(reader.TokenType + "\t" + reader.ValueType + "\t" + reader.Value);
+        //    //}
+        //    Console.WriteLine(reader.Value);
+        //    Console.Read();
+        //    //jt.Read();
+        //    //var value = jt.Value;
+        //    //return 
+        //}
+
+        //public static void ReadJson()
+        //{    
+        //    try
+        //    {
+        //        StreamReader sr = new StreamReader(jspath);
+        //        JsonTextReader jst = new JsonTextReader(sr);
+        //        JObject m_readjson = (JObject)JToken.ReadFrom(jst);
+        //        string res = m_readjson["employees"][1]["firstName"].ToString();
+        //        Console.WriteLine(res);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e.Message);
+        //        throw;
+        //    }
+        //    Console.ReadKey();
+        //}
+     
+       
+        public static void sReadJson()
+        {
+            // string jspath = "d://Configuration//js.json";
+
             try
             {
                 StreamReader sr = new StreamReader(jspath);
---
+++ C#/solution/jsondemo/jsondemo/demo1.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsondemo
+{
+    class demo1
+    {
+        private static readonly string jspath1 = "d://Configuration//js.json";
+        public static void ReadJson()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(jspath1);
+                JsonTextReader jst = new JsonTextReader(sr);
+                JObject m_readjson = (JObject)JToken.ReadFrom(jst);
+                string res = m_readjson["employees"][0]["firstName"].ToString();
+                Console.WriteLine(res);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            Console.ReadKey();     
+        }
+        public static void ReadJson1()
+        {
+
+            using (StreamReader sr1 = new StreamReader(jspath1))
+            {
+                using (JsonTextReader jst1 = new JsonTextReader(sr1))
+                {
+                    //JObject m_readjson1 = (JObject)JToken.ReadFrom(jst1);
+                    //string res = m_readjson1["empolyees"][0]["firstName"].ToString();
+                    //Console.WriteLine(res);
+                   
+                    JObject m_readjson1 = (JObject)JToken.ReadFrom(jst1);
+                    string res = m_readjson1["employees"][0]["firstName"].ToString();
+                    Console.WriteLine(res);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
