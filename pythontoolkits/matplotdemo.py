3f905584096d84033111506b16995476454bd05c
neo <1733112322@qq.com>
2020-03-31 14:39:40 +0800

    list update

--- pythontoolkits/matplotdemo.py
+++ pythontoolkits/matplotdemo.py
@@ -6,11 +6,21 @@
 from matplotlib import  pyplot as plt
 import  numpy as np
 # plt.switch_backend('agg')
-
+'''
 rng = np.random.RandomState(1)
 x = 10 * rng.rand(50)
 y = 2 * x - 5 + rng.randn(50)
 
 print(plt.get_backend())
 plt.scatter(x, y)
-plt.show()
+plt.show()'''
+if __name__=='__main__':
+    aa= []
+    b=[1,2,3]
+
+    aa.extend(b)
+    aa.extend(aa)
+    aa.extend(aa)
+    print(aa)
+
+
