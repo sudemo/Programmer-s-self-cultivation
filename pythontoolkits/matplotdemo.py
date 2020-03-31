44cb4271cbc42fa27b79ddc2b885b73e5cf71cbe
neo <1733112322@qq.com>
2020-03-31 19:33:22 +0800

    复制sheet数据

--- pythontoolkits/matplotdemo.py
+++ pythontoolkits/matplotdemo.py
@@ -16,11 +16,51 @@ plt.scatter(x, y)
 plt.show()'''
 if __name__=='__main__':
     aa= []
-    b=[1,2,3]
 
-    aa.extend(b)
-    aa.extend(aa)
-    aa.extend(aa)
-    print(aa)
+    s11 = []; s111 = []; s111 = []
+    s1=s2=s3=[]
+    b=s1=[1,2,3]
+    s1.insert(len(s1),11)
+    # aa.extend(b)
+    # aa.extend(aa)
+    # aa.extend(aa)
+    # print(s2,s1)
+    import xlwings as xw
+    filepath = "cupWeight2.csv"  # 与程序同目录下
+
+    filepath2 = "C1.xlsx"
+    cupwb1 = xw.Book(filepath)
+    swb2 =xw.Book(filepath2)
+    cupwb1 = xw.App(visible=False, add_book=False).books.open(filepath)
+    swb2 = xw.App(visible=False, add_book=False).books.open(filepath2)
+    try:
+        # sum2sht = wb2.sheets("模板").range('A1').expand().value
+        sum2sht = swb2.sheets("模板")
+        # num=wb2.sheets('模板').range(1, 1).expand().shape
+        sheet1= cupwb1.sheets[0]
+        # sheet1.range('a1').expand('down').value = rng
+        '''
+        批量复制整个
+        '''
+        my_values = swb2.sheets['模板'].range('A1:am11').options(ndim=2).value
+        cupwb1.sheets[0].range('A1:am11').value = my_values
+        cupsht = cupwb1.sheets[0]
+        wb1.app.calculate('automatic')
+        # cupsht.api.Copy(Before = sum2sht.api)
+
+        # print(cupsht)
+        # cell_index = str(rng.rows.count+1)
+        # range1 = wb2.sheets("Sheet1").range('A'+cell_index)
+        # range1.value = wb1.sheets("Sheet1").range('A1').expand('table').value
+        cupwb1.save()
+        cupwb1.close()
+        swb2.close()
+    except Exception as ex:
+        print(ex)
+        cupwb1.close()
+        swb2.close()
+        # xw.apps.quit()
 
 
+
+    # xw.apps.quit()
--- pythontoolkits/pyexcel.py
+++ pythontoolkits/pyexcel.py
@@ -77,7 +77,6 @@ def copytonewsheet(arg):
     work_detail = xw.Range(rg).value  # 选中范围的值
 
 
-
 if __name__ == '__main__':
     print("starting")
     sheetname = datetime.datetime.now().strftime('%m-%d')
@@ -95,19 +94,80 @@ if __name__ == '__main__':
         sum_sheet = summary_wb.sheets[sheetname]
         # fuzhi
         #  cup_num
-        line =cup_sheet.used_range.last_cell.row
+        # line =cup_sheet.used_range.last_cell.row
+        head = ['序号','空杯1',	'满杯1','净重1',	'空杯2','满杯2',	'净重2','空杯3',	'满杯3',
+                '净重3','空杯4', '满杯4','净重4','空杯5','满杯5','净重5',	'空杯6','满杯6',	'净重6']
+        sum_sheet.range('u1:am1').value = head
+
         index = cup_sheet.range('A2').expand('down').value  # 整列
         # print(index)
         c = 1
-        ss =[]
+        s1 = []; s11 = []; s111 = []  # 1满杯 11空杯 111净重
+        s2 = []; s22 = []; s222 = []
+        s3 = []; s33 = []; s333 = []
+        s4 = []; s44 = []; s444 = []
+        s5 = []; s55 = []; s555 = []
+        s6 = []; s66 = []; s666 = []
         for i in index:
             c = c + 1
-            if i == 1 and c < 21:
+            # if i == 1 and c < 21:
+            if i == 1:
+                s1.insert(len(s1),cup_sheet.range('b'+str(c)).value)  # 提取1号空杯
+                # print(s1)
+                s11.insert(len(s11),cup_sheet.range('c'+str(c)).value)  # 提取号满杯
+                s111.insert(len(s111),cup_sheet.range('d'+str(c)).value)  # 提取1号净重
+                # print(c)
+            if i == 2:
+                s2.insert(len(s2), cup_sheet.range('b' + str(c)).value)
+                # print(s1)
+                s22.insert(len(s22), cup_sheet.range('c' + str(c)).value)
+                s222.insert(len(s222), cup_sheet.range('d' + str(c)).value)
+            if i == 3:
+                s3.insert(len(s3), cup_sheet.range('b' + str(c)).value)
+                s33.insert(len(s33), cup_sheet.range('c' + str(c)).value)
+                s333.insert(len(s333), cup_sheet.range('d' + str(c)).value)
+            if i == 4:
+                s4.insert(len(s4), cup_sheet.range('b' + str(c)).value)
+                # print(s1)
+                s44.insert(len(s44), cup_sheet.range('c' + str(c)).value)
+                s444.insert(len(s444), cup_sheet.range('d' + str(c)).value)
+            if i == 5:
+                s5.insert(len(s5), cup_sheet.range('b' + str(c)).value)
+                # print(s1)
+                s55.insert(len(s55), cup_sheet.range('c' + str(c)).value)
+                s555.insert(len(s555), cup_sheet.range('d' + str(c)).value)
+            if i == 6:
+                s6.insert(len(s6), cup_sheet.range('b' + str(c)).value)
+                # print(i)
+                s66.insert(len(s66), cup_sheet.range('c' + str(c)).value)
+                s666.insert(len(s666), cup_sheet.range('d' + str(c)).value)
 
-                ss.append( cup_sheet.range('b'+str(c)+':'+'d'+str(c)).value)  # 提取1号空杯
-                print(c)
-        sum_sheet.range('v2:x2').options(transpose=True).value = ss
+        sum_sheet.range('v2').options(transpose=True).value = s1
+        sum_sheet.range('w2').options(transpose=True).value = s11
+        sum_sheet.range('x2').options(transpose=True).value = s111
 
+        sum_sheet.range('y2').options(transpose=True).value = s2
+        sum_sheet.range('z2').options(transpose=True).value = s22
+        sum_sheet.range('AA2').options(transpose=True).value = s222
+
+        sum_sheet.range('AB2').options(transpose=True).value = s3
+        sum_sheet.range('AC2').options(transpose=True).value = s33
+        sum_sheet.range('AD2').options(transpose=True).value = s333
+
+        sum_sheet.range('AE2').options(transpose=True).value = s4
+        sum_sheet.range('AF2').options(transpose=True).value = s44
+        sum_sheet.range('AG2').options(transpose=True).value = s444
+
+        sum_sheet.range('AH2').options(transpose=True).value = s5
+        sum_sheet.range('AI2').options(transpose=True).value = s55
+        sum_sheet.range('AJ2').options(transpose=True).value = s555
+
+        sum_sheet.range('AK2').options(transpose=True).value = s6
+        sum_sheet.range('AL2').options(transpose=True).value = s66
+        sum_sheet.range('AM2').options(transpose=True).value = s666
+
+
+        print('ok')
 
         # last_row_index = cupweight_wb.sheet[0].Range('A2').expand('table').last_cell.row  # 这表格最后一列
     except  Exception as ex:
