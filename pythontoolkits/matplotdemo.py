44a416e7c0a6eeedf02e450bc1d7845a2e29556f
neo <1733112322@qq.com>
2020-04-01 15:56:06 +0800

    复制sheet数据  同一个表内全复制

--- pythontoolkits/matplotdemo.py
+++ pythontoolkits/matplotdemo.py
@@ -26,26 +26,39 @@ if __name__=='__main__':
     # aa.extend(aa)
     # print(s2,s1)
     import xlwings as xw
-    filepath = "cupWeight2.csv"  # 与程序同目录下
+    # print(xw.__version__)
+    filepath = "cupWeight2.xlsx"  # 与程序同目录下
 
     filepath2 = "C1.xlsx"
     cupwb1 = xw.Book(filepath)
     swb2 =xw.Book(filepath2)
-    cupwb1 = xw.App(visible=False, add_book=False).books.open(filepath)
-    swb2 = xw.App(visible=False, add_book=False).books.open(filepath2)
+    # cupwb1 = xw.App(visible=False, add_book=False).books.open(filepath)
+    # swb2 = xw.App(visible=False, add_book=False).books.open(filepath2)
     try:
         # sum2sht = wb2.sheets("模板").range('A1').expand().value
-        sum2sht = swb2.sheets("模板")
+        sum2sht = swb2.sheets["模板"]
         # num=wb2.sheets('模板').range(1, 1).expand().shape
-        sheet1= cupwb1.sheets[0]
-        # sheet1.range('a1').expand('down').value = rng
+        sheet1= cupwb1.sheets['new sheet1']
+        fw = sum2sht.formula
+        sheet1.range('a1').value = sum2sht.range('a1').expand('table').value
+
+
+        # new_sheet_name = 'new sheet1'
+        # sht.api.Name = new_sheet_name
+        # sheet1.api.Columns(1).Copy(sum2sht.api.Columns(1))
+        # sheet1.range('B1').api.EntireColumn.Delete()
+        # ss = xw.Range('a1:c2').api.Copy
+        # print(ss)
+        #
+        # sheet1.range('a1').expand('down').value = ss
         '''
         批量复制整个
         '''
-        my_values = swb2.sheets['模板'].range('A1:am11').options(ndim=2).value
-        cupwb1.sheets[0].range('A1:am11').value = my_values
-        cupsht = cupwb1.sheets[0]
-        wb1.app.calculate('automatic')
+        # my_values = swb2.sheets['模板'].range('A1:am11').options(ndim=2).value
+        # cupwb1.sheets[0].range('A1:am11').value = my_values
+        # cupsht = cupwb1.sheets[0]
+        # cupwb1.app.calculate='automatic'
+
         # cupsht.api.Copy(Before = sum2sht.api)
 
         # print(cupsht)
@@ -53,8 +66,11 @@ if __name__=='__main__':
         # range1 = wb2.sheets("Sheet1").range('A'+cell_index)
         # range1.value = wb1.sheets("Sheet1").range('A1').expand('table').value
         cupwb1.save()
+        swb2.save()
         cupwb1.close()
         swb2.close()
+        # cupwb1.app.quit()
+        # swb2.app.quit()
     except Exception as ex:
         print(ex)
         cupwb1.close()
