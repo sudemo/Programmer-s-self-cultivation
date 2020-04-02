73ab870729454576ddbb8268ba6470b1c5bef63b
neo <1733112322@qq.com>
2020-04-02 15:33:53 +0800

    带格式的复制 调用api.Copy

--- pythontoolkits/matplotdemo.py
+++ pythontoolkits/matplotdemo.py
@@ -15,68 +15,55 @@ print(plt.get_backend())
 plt.scatter(x, y)
 plt.show()'''
 if __name__=='__main__':
-    aa= []
-
-    s11 = []; s111 = []; s111 = []
-    s1=s2=s3=[]
-    b=s1=[1,2,3]
-    s1.insert(len(s1),11)
+    # aa= []
+    #
+    # s11 = []; s111 = []; s111 = []
+    # s1=s2=s3=[]
+    # b=s1=[1,2,3]
+    # s1.insert(len(s1),11)
     # aa.extend(b)
     # aa.extend(aa)
     # aa.extend(aa)
     # print(s2,s1)
     import xlwings as xw
+    import time
+    from tqdm import tqdm
     # print(xw.__version__)
     filepath = "cupWeight2.xlsx"  # 与程序同目录下
 
     filepath2 = "C1.xlsx"
-    cupwb1 = xw.Book(filepath)
-    swb2 =xw.Book(filepath2)
-    # cupwb1 = xw.App(visible=False, add_book=False).books.open(filepath)
-    # swb2 = xw.App(visible=False, add_book=False).books.open(filepath2)
+    # cupwb1 = xw.Book(filepath)
+    # swb2 = xw.Book(filepath2)
+    # cupwb1 = xw.App(visible=True, add_book=False).books.open(filepath)
+    swb2 = xw.App(visible=True, add_book=False).books.open(filepath2)
+    #
     try:
-        # sum2sht = wb2.sheets("模板").range('A1').expand().value
+    #     # sum2sht = wb2.sheets("模板").range('A1').expand().value
+
         sum2sht = swb2.sheets["模板"]
-        # num=wb2.sheets('模板').range(1, 1).expand().shape
-        sheet1= cupwb1.sheets['new sheet1']
-        fw = sum2sht.formula
-        sheet1.range('a1').value = sum2sht.range('a1').expand('table').value
+
+    #     # num=wb2.sheets('模板').range(1, 1).expand().shape
+    #     sht1= swb2.sheets['Sheet1']
+
+        # cupsheet= cupwb1.sheets[1]
+        # cupsheet.range('v2:e5').clear_contents()
+        sum2sht.api.Copy(Before=sum2sht.api)
+        sht1 = swb2.sheets['模板 (2)']
+        sht1.name ='0402'
+        sht12 =swb2.sheets['0402']
+        sht12.range('v2').expand('table').clear_contents()
+        pbar = tqdm(total=100)
+        for i in range(10):
+            pbar.update(1)
+        time.sleep(0.1)
+        pbar.close()
 
 
-        # new_sheet_name = 'new sheet1'
-        # sht.api.Name = new_sheet_name
-        # sheet1.api.Columns(1).Copy(sum2sht.api.Columns(1))
-        # sheet1.range('B1').api.EntireColumn.Delete()
-        # ss = xw.Range('a1:c2').api.Copy
-        # print(ss)
-        #
-        # sheet1.range('a1').expand('down').value = ss
-        '''
-        批量复制整个
-        '''
-        # my_values = swb2.sheets['模板'].range('A1:am11').options(ndim=2).value
-        # cupwb1.sheets[0].range('A1:am11').value = my_values
-        # cupsht = cupwb1.sheets[0]
-        # cupwb1.app.calculate='automatic'
-
-        # cupsht.api.Copy(Before = sum2sht.api)
-
-        # print(cupsht)
-        # cell_index = str(rng.rows.count+1)
-        # range1 = wb2.sheets("Sheet1").range('A'+cell_index)
-        # range1.value = wb1.sheets("Sheet1").range('A1').expand('table').value
-        cupwb1.save()
         swb2.save()
-        cupwb1.close()
-        swb2.close()
-        # cupwb1.app.quit()
-        # swb2.app.quit()
+
     except Exception as ex:
         print(ex)
-        cupwb1.close()
+
         swb2.close()
-        # xw.apps.quit()
 
 
-
-    # xw.apps.quit()
--- pythontoolkits/pyexdemo1.py
+++ pythontoolkits/pyexdemo1.py
@@ -1,103 +1,54 @@
 '''
 # @Time    : 2020/3/12 9:14
 # @Author  : Neo
-# @File    : pyexcel.py
+# @File    : pyexdemo1.py
 '''
-import xlwings as xw;
-import time
+import xlwings as xw
+
+# import time
 import datetime
-# xlapp = xw.App(visible=True , add_book=False)
-# filepath = "D:\项目资料\example.xlsx"
+# from tqdm import tqdm
+
+
 # def openexcel(filename):
 filepath = "cupWeight.csv"  # 与程序同目录下
 filepath2 = "称重数据汇总（含模板）-C1.xlsx"
+# filepath2 = "C1.xlsx"
 # 打开一个表格
 try:
     # 实例化一个book
+    cupweight = ''
     cupweight_wb = xw.App(visible=False, add_book=False).books.open(filepath)
-    summary_wb = xw.App(visible=False, add_book=False).books.open(filepath2)
-    # cupweight_wb = xlapp.books.open(filepath)
-    # summary_wb = xlapp.books.open(filepath2)
+    smmary_wb = xw.App(visible=True, add_book=False).books.open(filepath2)
+    # swb2 = xw.App(visible=True, add_book=False).books.open(filepath2)
+    print('open xsl')
 except:
 
     print("找不到名为cupWeight/称重数据汇总（含模板）-C1 的文件")
-# 显示打开的表格名字
-# print(xlapp.books.active)
-
-
-def creatnewsheet(sheet_name):
-    # 创建新的sheet
-    # c_alphabeta_list = [chr(i) for i in range(65, 65 + 26)]
-    # l_alphabeta_list = [chr(i) for i in range(97, 97 + 26)]
-    key =input("是否需要创建新的sheet y/n?")
-
-    print(key)
-    # time.sleep(0.1)
-
-    if key == "y":
-
-        try:
-            ws = summary_wb.sheets.add()  # 新增数据表
-            ws.name = sheet_name
-            ws = summary_wb.sheets[sheet_name]
-            # rg = ws.range('B1')
-            # # rg.value = l_alphabeta_list
-            # rg = ws.range('A2')
-            # rg.value = [[chr(i)] for i in range(97, 97 + 26)]
-            summary_wb.save()
-            # summary_wb.close()
-        except():
-            print("添加sheet失败")
-            summary_wb.close()
-            cupweight_wb.close()
-    else:
-        print("cancle")
-        pass
-
-def copytonewsheet(arg):
-    # raw_cup_data = cupweight_wb.Range ("A1:B3")
-    # raw_sum_data = summary_wb.sheets[0]
-    # raw_sum_data.value =
-
-    '''raw_cup_data =cupweight_wb.sheets['cupWeight']  # 要复制的sheet
-    # 重点是copy
-    raw_cup_data.api.Copy(Before=raw_cup_data.api)
-    raw_sum_data = summary_wb.sheets[arg]
-    raw_sum_data.value = raw_cup_data
-    # print(raw_cup_data,raw_sum_data)'''
-
-    last_row_index = cupweight_wb.sheet[0].Range('A2').expand('table').last_cell.row  # 这表格最后一列
-
-    print(last_row_index)
-
-    rg = "A2:E" + str(last_row_index)  # 范围
-
-    print(rg)
-
-    work_detail = xw.Range(rg).value  # 选中范围的值
-
 
 if __name__ == '__main__':
     print("starting")
     sheetname = datetime.datetime.now().strftime('%m-%d')
-    '''
-    # print(sheetname)
-    creatnewsheet(sheetname)
-    # raw_sum_data = summary_wb.sheets[sheetname]
-    copytonewsheet(sheetname)
-    summary_wb.save()'''
-
 
     try:
+        sum2sht = smmary_wb.sheets["模板"]
+        # sum2sht.api.Copy(Before=sum2sht.api)
+        # sum2sht.api.Name = '3111'
         cup_sheet = cupweight_wb.sheets[0]
-        summary_wb.sheets.add(sheetname)
-        sum_sheet = summary_wb.sheets[sheetname]
-        # fuzhi
-        #  cup_num
+
+        # print(xw.Range('A1:E7')==smmary_wb.sheets[1].range('A1:E7')) # 这两个是同一个意思
+
+        sum2sht.api.Copy(Before=sum2sht.api)
+        shtcopy = smmary_wb.sheets['模板 (2)']
+        print('rename', sheetname)
+        shtcopy.name = sheetname  # 这个是要用的sheet
+
+        sum_sheet = smmary_wb.sheets[sheetname]
+        sum_sheet.range('v2').expand('table').clear_contents()  # 清除模板的杯子数据
         # line =cup_sheet.used_range.last_cell.row
-        head = ['序号','空杯1',	'满杯1','净重1',	'空杯2','满杯2',	'净重2','空杯3',	'满杯3',
-                '净重3','空杯4', '满杯4','净重4','空杯5','满杯5','净重5',	'空杯6','满杯6',	'净重6']
-        sum_sheet.range('u1:am1').value = head
+        # head = ['序号','空杯1',	'满杯1','净重1',	'空杯2','满杯2',	'净重2','空杯3',	'满杯3',
+        #         '净重3','空杯4', '满杯4','净重4','空杯5','满杯5','净重5',	'空杯6','满杯6',	'净重6']
+        # sum_sheet.range('u1:am1').value = head
 
         index = cup_sheet.range('A2').expand('down').value  # 整列
         # print(index)
@@ -108,7 +59,11 @@ if __name__ == '__main__':
         s4 = []; s44 = []; s444 = []
         s5 = []; s55 = []; s555 = []
         s6 = []; s66 = []; s666 = []
+        # pbar = tqdm(total=10)
+        print('start copying')
         for i in index:
+        #     pbar.update(1)
+        #
             c = c + 1
             # if i == 1 and c < 21:
             if i == 1:
@@ -165,25 +120,13 @@ if __name__ == '__main__':
         sum_sheet.range('AK2').options(transpose=True).value = s6
         sum_sheet.range('AL2').options(transpose=True).value = s66
         sum_sheet.range('AM2').options(transpose=True).value = s666
-
-
-        print('ok')
-
-        # last_row_index = cupweight_wb.sheet[0].Range('A2').expand('table').last_cell.row  # 这表格最后一列
+        print('copy finished')
+        smmary_wb.save()
+        # time.sleep(1)
+        cupweight_wb.close()
+        # smmary_wb.close()
     except  Exception as ex:
-        print("first error",ex)
+        print("first error", ex)
         cupweight_wb.close()
-        summary_wb.close()
-
-
-
-    try:
-        summary_wb.save()
-        cupweight_wb.close()
-        summary_wb.close()
-    except Exception as es:
-        print('last',es)
-        cupweight_wb.close()
-        summary_wb.close()
-
+        smmary_wb.close()
 
