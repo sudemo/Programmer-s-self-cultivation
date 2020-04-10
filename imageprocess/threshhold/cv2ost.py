a86ed835ecba7168c47f912d6eff77b2ca0bad25
neo <1733112322@qq.com>
2020-04-10 15:11:38 +0800

    imread in bgr plt.show in rgb

--- imageprocess/threshhold/cv2ost.py
+++ imageprocess/threshhold/cv2ost.py
@@ -34,19 +34,24 @@ cv2.imshow('gray', gray)
 # plt.title("source image"), plt.xticks([]), plt.yticks([])
 # plt.subplot(132), plt.hist(image.ravel(), 256)
 # plt.title("Histogram"), plt.xticks([]), plt.yticks([])
-
+#双边滤波
 imgviewx = cv2.bilateralFilter(gray, 0, 50, 6)
 
 # ret1, th1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)  #方法选择为THRESH_OTSU
-ret2, th2 = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# ret2, th2 = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+#自适应阈值 只返回分割后的图片
+th2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,3,3)
+# th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
 
 #显示文字
 # 参数：图像，文字内容， 坐标( x , y ) ，字体，大小，颜色( B , G ,R )，字体厚度
-font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-imgviewx = cv2.putText(imgviewx,"hello neo",(10, 50), font, 4, (0, 0, 23), 10)
-print(ret2,th2)
+# font = cv2.FONT_HERSHEY_PLAIN
+# imgviewx = cv2.putText(imgviewx,"hello neo",(30, 100), font, 4, (112, 122, 123), 10)
+# print(ret2,th2)
 cv2.imshow('th2',th2)
-cv2.imshow('1',imgviewx)
+# cv2.imshow('1',imgviewx)
 cv2.waitKey(0)
 # plt.figure()
 # plt.subplot(133),
