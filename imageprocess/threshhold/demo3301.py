42c0c126abb9811c782f037e6d0a325b68a950ae
neo <1733112322@qq.com>
2020-05-06 10:47:25 +0800

    test

--- imageprocess/threshhold/demo3301.py
+++ imageprocess/threshhold/demo3301.py
@@ -4,7 +4,7 @@
 @time: 2019/4/27 16:48
 '''
 
-
+import cv2
 def fun1(arg):
     ad = "sd"
     arg = eval(arg)
@@ -58,6 +58,17 @@ if __name__ == "__main__":
     # print(fun1("ad"))
     # print( add_end())
     # print(add_end())
-    print(add_end2())
-    add_end2()
+    # print(add_end2())
+    # add_end2()
     # print(help(add_end().__doc__))
+    # img = cv2.imread('c:\\用户\\Neo\\OneDrive\\图片\\11.bmp', 0)
+    img=cv2.imread('11.bmp',0)
+
+    if img is not None:
+        # cv2.imshow('0',img)
+        w,h = img.shape[:2]
+        shrink=cv2.resize(img, (int(w*0.3), int(h*0.3)), interpolation=cv2.INTER_CUBIC)
+        cv2.imshow('11',shrink)
+        cv2.waitKey(0)
+        exit(0)
+    exit(2)
