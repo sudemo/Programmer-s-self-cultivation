21adada00c52ef6ee4e247fed5441157c5f95157
neo <1733112322@qq.com>
2020-05-07 15:51:56 +0800

    opencvdemo

--- QT/facedemo/main.cpp
+++ QT/facedemo/main.cpp
@@ -1,4 +1,4 @@
-#include <QCoreApplication>
+﻿#include <QCoreApplication>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
@@ -11,11 +11,12 @@ int main()
 
 
         Mat image, image_gray;      //定义两个Mat变量，用于存储每一帧的图像
+        string path = "d://2121809.png";
+        string path1 = "d://lena.jpg";
+        image = imread(path1);
+//        imshow("oringin", image);
 
-        image = imread("d://2.jpg");
-        imshow("oringin", image);
-
-        cvtColor(image, image_gray, CV_BGR2GRAY);//转为灰度图
+        cvtColor(image, image_gray, COLOR_BGR2GRAY);//转为灰度图
         equalizeHist(image_gray, image_gray);//直方图均衡化，增加对比度方便处理
 
         CascadeClassifier eye_Classifier;  //载入分类器
@@ -23,15 +24,16 @@ int main()
 
         //加载分类训练器，OpenCv官方文档提供的xml文档，可以直接调用
         //xml文档路径  opencv\sources\data\haarcascades
-        if (!eye_Classifier.load("D:\\program\\opencv\\opencv\\sources\\haarcascades\\haarcascade_eye.xml"))  //需要将xml文档放在自己指定的路径下
+//        D:\program\opencv\opencv4\opencv\sources\data\haarcascades
+        if (!eye_Classifier.load("D:\\program\\opencv\\opencv4\\opencv\\sources\\data\\haarcascades\\haarcascade_eye.xml"))  //需要将xml文档放在自己指定的路径下
         {
-            cout << "Load haarcascade_eye.xml failed!" << endl;
+            cout << "Load haarcascade_eye.xml failed!1" << endl;
             return 0;
         }
 
-        if (!face_cascade.load("D:\\program\\opencv\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt.xml"))
+        if (!face_cascade.load("D:\\program\\opencv\\opencv4\\opencv\\sources\\data\\haarcascades\\haarcascade_eye.xml"))
         {
-            cout << "Load haarcascade_frontalface_alt failed!" << endl;
+            cout << "Load haarcascade_frontalface_alt failed!2" << endl;
             return 0;
         }
 
@@ -40,20 +42,20 @@ int main()
         vector<Rect> faceRect;
 
         //检测关于眼睛部位位置
-        eye_Classifier.detectMultiScale(image_gray, eyeRect, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+        eye_Classifier.detectMultiScale(image_gray, eyeRect, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
         for (size_t eyeIdx = 0; eyeIdx < eyeRect.size(); eyeIdx++)
         {
-            rectangle(image, eyeRect[eyeIdx], Scalar(0, 0, 255));   //用矩形画出检测到的位置
+            rectangle(image, eyeRect[eyeIdx], Scalar(0, 255, 0));   //用矩形画出检测到的位置
         }
 
         //检测关于脸部位置
-        face_cascade.detectMultiScale(image_gray, faceRect, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+        face_cascade.detectMultiScale(image_gray, faceRect, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
         for (size_t i = 0; i < faceRect.size(); i++)
         {
-            rectangle(image, faceRect[i], Scalar(0, 0, 255));      //用矩形画出检测到的位置
+            rectangle(image, faceRect[i], Scalar(0, 0, 155));      //用矩形画出检测到的位置
         }
-
-        imshow("test", image);         //显示当前帧
+        namedWindow("1",0);
+        imshow("1", image);         //显示当前帧
         waitKey(0);
         return 0;
 
---
+++ QT/opdemo2/mainwindow.cpp
@@ -0,0 +1,59 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QWidget>
+#include <QFileDialog>
+
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_pressed()
+{
+    QString filename = QFileDialog::getOpenFileName(this,
+                                                    "open file",
+                                                    QDir::currentPath(),
+                                                    "Image(*.jpg)");
+//if (QFile::exists(filename))
+//{
+////    ui->inputLineEdit.setText(filename);
+//    ui->lineEdit->setText(filename);
+
+//}
+    if (!filename.isEmpty())
+    {
+        ui->lineEdit->setText(filename);
+        using namespace cv;
+        Mat inputImg,outImg;
+        inputImg = imread(ui->lineEdit->text().toStdString());
+        if (ui->radioButton->isChecked())
+            cv::medianBlur(inputImg,outImg,(5,5));
+            cv::imshow("out",outImg);
+
+    }
+
+
+
+}
+
+//void MainWindow::on_pushButton_clicked()
+//{
+//    ui->lineEdit->setText("clicked");
+
+//}
+
+
