ba0ccd2a185d85a35f132162871904291c8d241b
neo <1733112322@qq.com>
2020-06-05 15:41:01 +0800

    blogdemo1

---
+++ web/myblogdemo/myblog/serializers.py
@@ -0,0 +1,19 @@
+'''
+# @Time    : 2020/6/2 14:06
+# @Author  : Neo
+# @File    : serializers.py
+'''
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
