852f5c41c2050c8685017744b2a4d14793644a03
neo <1733112322@qq.com>
2020-06-23 09:49:36 +0800

    myapp

---
+++ web/appdemo/myapp/myapp/urls.py
@@ -0,0 +1,31 @@
+"""myapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from hello import views as hello_views
+from calculator import  views as cviews
+from exam import views as eviews
+from migratesqldemo import views as mviews
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("1/",hello_views.index),
+    path("add/",cviews.add,name = 'add'),
+    path('add/<int:a>/<int:b>/', cviews.add2, name='add2'),
+    path("11",eviews.get_name),
+    path('', mviews.get_datail),
+]
