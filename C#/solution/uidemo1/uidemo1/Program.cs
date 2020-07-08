03b748afa75fc74d33d612c09867f566e48f6f43
neo <1733112322@qq.com>
2020-07-08 11:13:22 +0800

    ces

--- C#/solution/uidemo1/uidemo1/Program.cs
+++ C#/solution/uidemo1/uidemo1/Program.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Runtime.InteropServices;
 namespace uidemo1
 {
     static class Program
     {
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AllocConsole();
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole();
+
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
         static void Main()
         {
+            AllocConsole();
+            Console.WriteLine("hellos");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+       
         }
     }
 }
