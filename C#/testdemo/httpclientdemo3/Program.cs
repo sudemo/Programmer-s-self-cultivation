3595217e12eeb7835969870e4d27eebd50b6b1d1
neo <1733112322@qq.com>
2020-07-21 09:53:29 +0800

    进程调用socket

---
+++ C#/testdemo/httpclientdemo3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Diagnostics;
+
+namespace httpclientdemo3
+{
+    class Program
+    {
+         static  void Main(string[] args)
+        {
+            var sss = db();
+            Console.WriteLine(sss.IsCompleted);
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            while (!sss.IsCompleted)
+            {
+                //Thread.Sleep(100);
+
+
+                Console.WriteLine(st.ElapsedTicks);
+                Console.WriteLine(sss.IsCompleted);
+            }
+            Console.WriteLine(st.ElapsedTicks);
+            Console.WriteLine(sss);
+                Console.ReadKey();
+        }
+
+        async static Task<string> db()
+        {
+            string s1 = "http://www.baidu.com/";
+            string s2 = "http://www.baidu1.com/";
+            HttpClient ht = new HttpClient();
+            //Task.Delay(10).Wait();
+            var ss = await ht.GetStringAsync(s1);
+            //Console.WriteLine(ss);
+            return ss;
+            //Console.ReadKey();
+        }
+
+    }
+}
---
+++ C#/testdemo/multisocketdemo/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using S7.Net;
+using PLCHelper;
+
+
+namespace multisocketdemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            socdemo1();
+
+        }
+
+
+        public void socdemo1()
+        {
+
+           var myclient1 =     PlcHelper.plcHelper_ins.GetInstance();
+            myclient1.Open();
+           var res1= myclient1.ReadDB(1,0,2);
+            this.richTextBox1.Text = res1.ToString();
+
+        }
+
+        public void socdemo2()
+        {
+
+            var myclient2 = PlcHelper.plcHelper_ins.GetInstance();
+            myclient2.Open();
+            var res2= myclient2.ReadDB(1,3,3);
+            this.richTextBox1.Text = res2.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            socdemo2();
+           
+        }
+    }
+}
