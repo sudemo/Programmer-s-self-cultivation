4358bdba5acd3078c2dd2de8bf807ef73e5b105c
neo <1733112322@qq.com>
2020-07-24 08:41:03 +0800

    plcdemo

---
+++ C#/testdemo/multisocketdemo/PLCHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using S7.Net;
+
+
+namespace PLCHelper
+
+{
+    class PlcHelper
+    {
+        private static CpuType c = CpuType.S71500;
+        public static string ip = "127.0.0.1";
+        //public static string ip = "192.168.0.10";
+        private Plc s7netplc_instance = new Plc(c, ip, 0, 1);
+
+        public static PlcHelper plcHelper_ins = new PlcHelper();
+
+
+
+        //public PlcHelper() : base(CpuType.S71500, ip, 0, 1) { }
+
+
+
+        //public PlcHelper()
+        //{
+        //    Plc myplc = new Plc(CpuType.S71500, "192.168.0.10", 0, 1);
+
+        //}
+
+        public PlcHelper GetInstance()
+        {
+            if (plcHelper_ins == null)
+            {
+                return plcHelper_ins = new PlcHelper();
+            }
+            else
+            {
+                return plcHelper_ins;
+            }
+        }
+
+
+        public void Open()
+        {
+            s7netplc_instance.Open();
+        }
+
+        public void Open_async()
+        {
+            s7netplc_instance.OpenAsync();//zhege 才是真的异步的地方，其内部是异步的，此处往上封装 的都不是异步
+        }
+
+        //默认读取DB，需要参数DB number, startaddress,count
+        public object ReadDB(int db, int startaddr, int count)
+        {
+            return s7netplc_instance.Read(DataType.DataBlock, db, startaddr, VarType.Byte, count);
+        }
+
+        public object ReadDB_dint(int db, int startaddr, int count)
+        {
+            return s7netplc_instance.Read(DataType.DataBlock, db, startaddr, VarType.DInt, count);
+        }
+        public object ReadDB_int(int db, int startaddr, int count)
+        {
+            return s7netplc_instance.Read(DataType.DataBlock, db, startaddr, VarType.Int, count);
+        }
+
+
+        //异步读
+        public Task<object> ReadDB_Async(int db, int startaddr, int count)
+        {
+            return s7netplc_instance.ReadAsync(DataType.DataBlock, db, startaddr, VarType.DWord, count);
+        }
+
+        public Task<byte[]> ReadDB_Bytes_Async(int db, int startaddr, int count)
+        {
+            return s7netplc_instance.ReadBytesAsync(DataType.DataBlock, db, startaddr, count);
+        }
+
+
+    }
+
+}
---
+++ C#/testdemo/multisocketdemo/Program.cs
@@ -0,0 +1,105 @@
+﻿using PLCHelper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multisocketdemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            //Thread th1 = new Thread(socdemo1);
+            //Thread th2 = new Thread(socdemo2);
+            //th2.Start();
+            int i = 0;
+            //string ip;
+            //IPAddress[] iparray;
+            //iparray = Dns.GetHostAddresses(Dns.GetHostName());
+            //IPAddress localip = iparray.First(ip =>ip.AddressFamily==AddressFamily.InterNetwork);
+            //Console.WriteLine(localip);
+            //var ip2 = "192.168.0.11";
+            //ProcessStartInfo st = new ProcessStartInfo();
+            //st.FileName = @"poweroff.exe";
+            //st.Arguments = ip2;
+            //st.CreateNoWindow = true;
+            //st.UseShellExecute = false;
+            ////var res =  Process.Start("poweroff.exe", ip2);  
+            //Process p = Process.Start(st);
+            //Thread.Sleep(3000);
+
+            mystartinfo();
+           // Console.ReadKey();
+
+        }
+
+
+
+        static void mystartinfo()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("poweroff.exe");
+            // startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+           // Process.Start(startInfo);
+
+            startInfo.Arguments = "127.0.0.1";
+
+            Process.Start(startInfo);
+        }
+
+
+        public static void socdemo1()
+        {
+
+            var myclient1 = PlcHelper.plcHelper_ins.GetInstance();
+            myclient1.Open();
+            object res1 = myclient1.ReadDB(1, 3, 3);
+            byte[] res = (byte[])res1;
+            foreach (short r in res)
+            {
+                Console.Write(r);
+            }
+            //Console.Write
+            // this.richTextBox1.Text = res2.ToString();
+        }
+
+        public static void socdemo2()
+        {
+
+            var myclient2 = PlcHelper.plcHelper_ins.GetInstance();
+            myclient2.Open();
+            var res2 = myclient2.ReadDB(1, 3, 3);
+            // this.richTextBox1.Text = res2.ToString();
+            byte[] res = (byte[])res2;
+            foreach (short r in res)
+            {
+                Console.Write(r);
+            }
+        }
+
+        public static void generate_soc()
+        {
+            List<PlcHelper> soc = new List<PlcHelper>();
+            for (int i = 0; i < 10; i++)
+
+            {
+                soc[i] = PlcHelper.plcHelper_ins.GetInstance();
+            }
+
+        }
+    }
+}
