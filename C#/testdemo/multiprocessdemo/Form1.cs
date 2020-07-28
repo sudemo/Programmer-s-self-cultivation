9fefa9ab75d7056a43d7994a27e4c990189b27e8
neo <1733112322@qq.com>
2020-07-28 10:43:07 +0800

    ipc

--- C#/testdemo/multiprocessdemo/Form1.cs
+++ C#/testdemo/multiprocessdemo/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,8 @@ namespace multiprocessdemo
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             //启动一个新的socket进程
@@ -39,6 +40,28 @@ namespace multiprocessdemo
 
         private void button2_Click(object sender, EventArgs e)
         {
+
+
+            Process piClient = new Process();
+
+            piClient.StartInfo.FileName = "SocketProcess.exe";
+            piClient.StartInfo.Arguments = "2";
+            using (AnonymousPipeServerStream pipeServer =
+                new AnonymousPipeServerStream(PipeDirection.Out,
+                HandleInheritability.Inheritable))
+            {
+                Console.WriteLine("[SERVER] Current TransmissionMode: {0}.",
+                    pipeServer.TransmissionMode);
+
+                // Pass the client process a handle to the server.
+                piClient.StartInfo.Arguments =
+                    pipeServer.GetClientHandleAsString();
+                piClient.StartInfo.UseShellExecute = false;
+                piClient.Start();
+
+                pipeServer.DisposeLocalCopyOfClientHandle();
+
+            }
 
         }
     }
