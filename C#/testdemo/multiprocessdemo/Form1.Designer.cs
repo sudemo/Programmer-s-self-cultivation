8a1fe1649319f06907bdba3fda70aeb407235c38
neo <1733112322@qq.com>
2020-07-30 09:20:21 +0800

    update

--- C#/testdemo/multiprocessdemo/Form1.Designer.cs
+++ C#/testdemo/multiprocessdemo/Form1.Designer.cs
@@ -30,11 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.process1 = new System.Diagnostics.Process();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(92, 101);
+            this.button1.Location = new System.Drawing.Point(29, 21);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,7 +50,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(92, 200);
+            this.button2.Location = new System.Drawing.Point(29, 74);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -52,11 +58,59 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-6, 262);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(794, 189);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(202, 117);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(373, 116);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            this.process1.Exited += new System.EventHandler(this.process1_Exited);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "open3#";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -64,6 +118,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -72,6 +127,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
---
+++ C#/testdemo/multiprocessdemo/Logger.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace logbaseQuene
+{
+    /// <summary>  
+    /// 日志类  
+    /// </summary>  
+    /// <remarks>  
+    /// 当调用Write方法时不会造成线程阻塞,而是立即完成方法调用,因此调用线程不用等待日志写入文件之后才返回。  
+    /// </remarks>  
+    public class Logger2
+    {
+        public static void Infor(string msgText)
+        {
+            Write(DateTime.Now, MsgType.Infor, "", msgText);
+        }
+        public static void Error(string msgText)
+        {
+            Write(DateTime.Now, MsgType.Error, "", msgText);
+        }
+        public static void Fatal(string msgText)
+        {
+            Write(DateTime.Now, MsgType.Fatal, "", msgText);
+        }
+        /// <summary>  
+        /// 写日志基础方法  
+        /// </summary>  
+        /// <param name="msgDataTime"></param>  
+        /// <param name="msgType"></param>  
+        /// <param name="msgLocation"></param>  
+        /// <param name="msgText"></param>  
+        private static void Write(DateTime msgDataTime, MsgType msgType, string msgLocation, string msgText)
+        {
+            QueueManager qm = new QueueManager();
+            qm.WriteToQueue(msgDataTime, msgType, msgLocation, msgText);
+        }
+
+        #region QueueManager
+        /// <summary>  
+        /// 企业应用框架的日志类，基于队列  
+        /// </summary>  
+        private class QueueManager : IDisposable
+        {
+            /// <summary>  
+            /// 日志对象的缓存队列  
+            /// </summary>  
+            private static Queue<Msg> _msgsQueue;
+
+            /// <summary>  
+            /// 日志文件保存的路径  
+            /// </summary>  
+            private static readonly string Path = BasePath + "\\AppLogs\\";
+            /// <summary>
+            /// Web和WinForm通用的取当前根目录的方法 
+            /// </summary>
+            private static string BasePath
+            {
+                get
+                {
+                    //if (System.Web.HttpContext.Current != null)
+                    //    return System.Web.HttpContext.Current.Server.MapPath("~/").TrimEnd(new char[] { '\\' });
+                    //else //当控件在定时器的触发程序中使用时就为空
+                    //{
+                    return AppDomain.CurrentDomain.BaseDirectory.TrimEnd(new char[] { '\\' });
+                    //}
+                }
+            }
+
+            /// <summary>  
+            /// 日志写入文件线程的控制标记,true为正在写入  
+            /// </summary>  
+            private static bool _state = false;
+
+            /// <summary>  
+            /// 日志文件生命周期的时间标记  
+            /// </summary>  
+            private static DateTime _timeSign;
+
+            /// <summary>  
+            /// 日志文件写入流对象  
+            /// </summary>  
+            private static StreamWriter _writer;
+            private delegate void WorkDelegate();
+            private static WorkDelegate _workDg;
+            /// <summary>  
+            /// 初始化  
+            /// </summary>  
+            public QueueManager()
+            {
+                if (_msgsQueue == null)
+                {
+                    if (!Directory.Exists(Path))
+                        Directory.CreateDirectory(Path);
+                    _msgsQueue = new Queue<Msg>();
+                    _workDg = new WorkDelegate(Work);
+                }
+            }
+            /// <summary>  
+            /// 写入新日志，根据指定的日志对象Msg  
+            /// </summary>  
+            /// <param name="msg">日志内容对象</param>  
+            private void WriteToQueueMethod(Msg msg)
+            {
+                if (msg != null)
+                {
+                    lock (_msgsQueue)
+                    {
+                        _msgsQueue.Enqueue(msg);
+                    }
+                }
+                if (_msgsQueue.Count > 0 && !_state)
+                {
+                    _state = true;
+                    _workDg.BeginInvoke(null, null);
+                    //以异步进行调用，也就是该方法封送完毕后马上返回，
+                    //不会等待委托方法的执行结束，调用者线程将不会被阻塞
+                }
+            }
+            /// <summary>  
+            /// 日志写入文件线程执行的方法,消费者  
+            /// </summary>  
+            private void Work()
+            {
+                //判断队列中是否存在待写入的日志  
+                while (_msgsQueue.Count > 0)
+                {
+                    Msg msg = null;
+                    lock (_msgsQueue)
+                    {
+                        msg = _msgsQueue.Dequeue(); //char ch = (char)q.Dequeue();将队列中的日志移除，并将其赋值给msg
+                    }
+                    if (msg != null)
+                    {
+                        WriteToFile(msg);
+                    }
+                }
+                _state = false;
+                FileClose();
+            }
+
+            /// <summary>  
+            /// 通过判断文件的到期时间标记将决定是否创建新文件。  
+            /// </summary>  
+            /// <returns></returns>  
+            private static string GetFilename()
+            {
+                DateTime now = DateTime.Now;
+                string format = "yyyy-MM-dd'.log'";
+                _timeSign = new DateTime(now.Year, now.Month, now.Day);
+                _timeSign = _timeSign.AddDays(1);
+                format = "ParentTH" + format;   //修改日志名字
+                return now.ToString(format);
+            }
+
+            /// <summary>  
+            /// 写入日志文本到文件的方法  
+            /// </summary>  
+            /// <param name="msg"></param>  
+            private void WriteToFile(Msg msg)
+            {
+                try
+                {
+                    if (_writer == null)
+                    {
+                        FileOpen();
+                    }
+                    //判断文件到期标志，如果当前文件到期则关闭当前文件创建新的日志文件  
+                    if (DateTime.Now >= _timeSign)
+                    {
+                        FileClose();
+                        FileOpen();
+                    }
+                    if (_writer != null)
+                    {
+                        _writer.WriteLine(string.Format("日志时间：{0}", msg.Datetime) + "\n日志级别：" + msg.Type + "\t" + msg.Location + "\n日志内容：" + msg.Text);
+                        _writer.Flush();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Out.Write(e);
+                }
+            }
+
+            //打开文件准备写入  
+            private void FileOpen()
+            {
+                _writer = new StreamWriter(Path + GetFilename(), true, Encoding.UTF8);
+            }
+
+            //关闭打开的日志文件  
+            private void FileClose()
+            {
+                if (_writer != null)
+                {
+                    _writer.Flush();
+                    _writer.Close();
+                    _writer.Dispose();
+                    _writer = null;
+                }
+            }
+
+
+            /// <summary>  
+            /// 将日志写入队列 写入新日志，根据指定的日志时间、日志内容和信息类型写入新日志  
+            /// </summary>  
+            /// <param name="msgDataTime"></param>
+            /// <param name="msgType"></param>
+            /// <param name="msgLocation"></param>
+            /// <param name="msgText"></param>  
+            public void WriteToQueue(DateTime msgDataTime, MsgType msgType, string msgLocation, string msgText)
+            {
+                WriteToQueueMethod(new Msg(msgDataTime, msgType, msgLocation, msgText));
+            }
+
+            #region IDisposable 成员
+
+            /// <summary>  
+            /// 销毁日志对象  
+            /// </summary>  
+            public void Dispose()
+            {
+                _state = false;
+            }
+
+            #endregion
+            #endregion
+
+        #region Msg
+
+            /// <summary>  
+            /// 表示一个日志记录的对象  
+            /// </summary>  
+            private class Msg
+            {
+                //日志记录的时间  
+                //日志记录的类型  
+                //日志记录的内容  
+
+                /// <summary>  
+                /// 创建新的日志记录实例;  
+                /// </summary>  
+                /// <param name="msgDataTime"></param>  
+                /// <param name="msgType"></param>  
+                /// <param name="msgLocation"></param>  
+                /// <param name="msgText"></param>  
+                public Msg(DateTime msgDataTime, MsgType msgType, string msgLocation, string msgText)
+                {
+                    Datetime = msgDataTime;
+                    Type = msgType;
+                    Location = msgLocation;
+                    Text = msgText;
+                }
+
+                /// <summary>  
+                /// 获取或设置日志记录的时间  
+                /// </summary>  
+                public DateTime Datetime { get; private set; }
+
+                public string Location { get; private set; }
+
+                /// <summary>  
+                /// 获取或设置日志记录的消息类型  
+                /// </summary>  
+                public MsgType Type { get; private set; }
+
+                /// <summary>  
+                /// 获取或设置日志记录的文本内容  
+                /// </summary>  
+                public string Text { get; private set; }                
+            }
+        }
+
+        #endregion
+
+        #region MsgType
+
+        /// <summary>  
+        /// 日志消息类型的枚举  
+        /// </summary>  
+        public enum MsgType
+        {
+            /// <summary>  
+            /// 普通信息类型的日志记录  
+            /// </summary>  
+            Infor,
+            /// <summary>  
+            /// 警告信息类型的日志记录  
+            /// </summary>  
+            Warning,
+            /// <summary>  
+            /// 错误信息类型的日志记录  
+            /// </summary>  
+            Error,
+            /// <summary>  
+            /// 成功信息类型的日志记录  
+            /// </summary>  
+            Success,
+            /// <summary>  
+            /// 致命类型的日志记录  
+            /// </summary>  
+            Fatal
+        }
+
+        #endregion
+    }
+}
