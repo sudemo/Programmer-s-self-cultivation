2eb72226167a236a93464d87cacdc0ebd7b4fe64
neo <1733112322@qq.com>
2020-08-19 14:57:41 +0800

    DelegateApp

--- C#/solution/delegatedemo/delegatedemo/Program.cs
+++ C#/solution/delegatedemo/delegatedemo/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 
-delegate int NumberChanger(int n);
 namespace DelegateAppl
 {
+    delegate int NumberChanger(int n);
+
+    public delegate int FT(int a);
     class TestDelegate
     {
+        public FT FtMethod;
         static int num = 10; //default =10
         public static int AddNum(int p) //定义一个方法，需要一个参数 int p,num = num + p
         {
@@ -25,15 +28,21 @@ namespace DelegateAppl
         static void Main(string[] args) //程序入口
         {
             // 创建委托实例
-            NumberChanger nc1 = new NumberChanger(AddNum);//带一个方法
-            NumberChanger nc2 = new NumberChanger(MultNum);
-            // 使用委托对象调用方法
-            nc1(25);
-            Console.WriteLine("Value of Num: {0}", getNum());
-            nc2(5);
-            Console.WriteLine("Value of Num: {0}", getNum());//此时num 的值已经为 35
-            Console.ReadKey();
+            //NumberChanger nc1 = new NumberChanger(AddNum);//带一个方法
+            //NumberChanger nc2 = new NumberChanger(MultNum);
+            //// 使用委托对象调用方法
+            //nc1(25);
+            //Console.WriteLine("Value of Num: {0}", getNum());
+            //nc2(5);
+            //Console.WriteLine("Value of Num: {0}", getNum());//此时num 的值已经为 35
+            //Console.ReadKey();
             //re
+            TestDelegate td = new TestDelegate();
+            td.FtMethod = AddNum;
+            td.FtMethod += MultNum;
+            Console.WriteLine( td.FtMethod(10));
+            Console.Read();
+
         }
     }
 }
