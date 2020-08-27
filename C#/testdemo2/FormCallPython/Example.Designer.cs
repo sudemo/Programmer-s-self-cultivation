97d696999412bb896991af941ad061a67917c026
neo <1733112322@qq.com>
2020-08-27 14:00:58 +0800

    FormCallPython

---
+++ C#/testdemo2/FormCallPython/Example.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ToolApp
+{
+    partial class Example
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(227, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(384, 167);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 2;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(288, 250);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 3;
+            this.add.Text = "add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(544, 337);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(73, 21);
+            this.textBox4.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(609, 122);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // Example
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Example";
+            this.Text = "Example";
+            this.Load += new System.EventHandler(this.Example_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ C#/testdemo2/FormCallPython/Example.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ToolApp
+{
+    public partial class Example : Form
+    {
+        private Process progressTest;
+
+        public Example()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 开始测试
+        /// </summary>
+        /// <param name="pathAlg">py文件路径</param>
+        /// <param name="a">加数a</param>
+        /// <param name="b">加数b</param>
+        /// <returns></returns>
+        public bool StartTest(string pathAlg, int a, int b)
+        {
+            bool state = true;
+
+            if (!File.Exists(pathAlg))
+            {
+                throw new Exception("The file was not found.");
+                //return false;
+            }
+            string sArguments = pathAlg;
+            //sArguments += " " + a.ToString() + " " + b.ToString() + " -u";//Python文件的路径用“/”划分比较常见
+            sArguments += " " + "f1" + " -u";
+            ProcessStartInfo ps = new ProcessStartInfo();
+            ps.FileName = "C:/Users/Neo/.conda/envs/exceldemo/python.exe";//环境路径需要配置好
+            //start.FileName = "python.exe";
+            ps.Arguments = sArguments;
+            ps.UseShellExecute = false;
+            ps.RedirectStandardOutput = true;
+            ps.RedirectStandardInput = true;
+            ps.RedirectStandardError = true;
+            ps.CreateNoWindow = true;
+
+            try
+            {
+
+
+
+                using (progressTest = Process.Start(ps))
+                {
+                   // _ = progressTest.TotalProcessorTime;
+                    // 异步获取命令行内容
+                    progressTest.BeginOutputReadLine();
+
+                    // 为异步获取订阅事件
+                    progressTest.OutputDataReceived += new DataReceivedEventHandler(outputDataReceived);
+                    progressTest.ErrorDataReceived += new DataReceivedEventHandler(outputDataReceived);
+                    //
+                }
+            }
+            catch (Exception e2)
+            {
+
+                MessageBox.Show(e2.Message);
+            }
+            //progressTest.WaitForExit();
+            return state;
+
+        }
+
+        public void outputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                this.Invoke(new Action(() =>
+                {
+                    this.textBox3.Text = e.Data;
+                }));
+            }
+            else
+            {
+                this.Invoke(new Action(() =>
+                {
+                    this.richTextBox1.AppendText("no data\n");
+                }));
+            }
+        }
+
+        private void Example_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //string path = Application.StartupPath + @"/demo2.py";//py文件路径
+                string path = "d:/S7demo1.py";
+                //string path = "D:/Gitrepository/github/Programmer self-cultivation/pythontoolkits/UniversalS7/S7demo1.py";
+                int a = Convert.ToInt32(this.textBox1.Text);
+                int b = Convert.ToInt32(this.textBox2.Text);
+                StartTest(path, a, b);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+            }
+
+        }
+    }
+}
