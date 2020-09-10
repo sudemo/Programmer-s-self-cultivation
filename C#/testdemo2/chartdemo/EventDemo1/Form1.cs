3cf3ad28d18a19d57e500f5c187b5e9488311b86
neo <1733112322@qq.com>
2020-09-10 11:27:05 +0800

    EventDemo1 监测 文件变动，但是会跨线程访问控件

---
+++ C#/testdemo2/chartdemo/EventDemo1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventDemo1
+{
+    public partial class Form1 : Form
+    {
+
+        //public string DisplayString 
+        public string DisplayString { get; set; }
+        public Form1()
+        {
+         //   Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //richTextBox1.Text = DisplayString;
+
+            //timer1.Interval = 1000;//以毫秒为单位
+            //timer1.Enabled = true;
+            FileChange.Run("C:\\Users\\Neo\\Desktop\\bat");
+
+        }
+
+
+
+        public void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine("changed " + e.ToString() + source.ToString());
+            Program.MainForm.DisplayString = "changed";
+            Program.MainForm.richTextBox1.AppendText("changed");
+            //Program.MainForm.Refresh();
+            //文件改變後的代碼
+        }
+
+        public void OnCreated(object source, FileSystemEventArgs e)
+        {
+            //添加文件後的代碼
+        }
+
+        public void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            //文件刪除後的代碼
+        }
+
+        public void OnRenamed(object source, RenamedEventArgs e)
+        {
+            //文件重命名後的代碼
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = DisplayString;
+        }
+
+
+
+        //timer1.Interval=3600000;//以毫秒为单位
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            //if (DisplayString != null)
+            //{
+            //    this.Invoke(new Action(() =>
+            //    richTextBox1.AppendText("hhs")));
+            //}
+        }
+    }
+
+    public class FileChange
+    {
+
+        public static void Run(string  path)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            //SlotMethods sm = new SlotMethods();
+            
+           
+                watcher.Path = path;//@"d:DownLoads";//args[1];
+
+                watcher.NotifyFilter = NotifyFilters.LastWrite;
+                //| NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                // Only watch text files.
+                watcher.Filter = "*.txt";
+            
+                // Add event handlers.
+                watcher.Changed += new FileSystemEventHandler(Program.MainForm.OnChanged);
+                //watcher.Created += new FileSystemEventHandler(sm.OnCreated);
+                //watcher.Deleted += new FileSystemEventHandler(sm.OnDeleted);
+                //watcher.Renamed += new RenamedEventHandler(sm.OnChanged);
+
+                // Begin watching.
+                watcher.EnableRaisingEvents = true;
+
+            
+        }
+
+        public static void Run(ArrayList path)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            //SlotMethods sm = new SlotMethods();
+            foreach (string s in path)
+            {
+
+                watcher.Path = s;//@"d:DownLoads";//args[1];
+
+                watcher.NotifyFilter = NotifyFilters.LastWrite;
+                //| NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                // Only watch text files.
+                watcher.Filter = "*.txt";
+
+                // Add event handlers.
+                //watcher.Changed += new FileSystemEventHandler(sm.OnChanged);
+                //watcher.Created += new FileSystemEventHandler(sm.OnCreated);
+                //watcher.Deleted += new FileSystemEventHandler(sm.OnDeleted);
+                //watcher.Renamed += new RenamedEventHandler(sm.OnChanged);
+
+                // Begin watching.
+                watcher.EnableRaisingEvents = true;
+
+            }
+        }
+    }
+   
+}
---
+++ C#/testdemo2/chartdemo/EventDemo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventDemo1
+{
+    static class Program
+    {
+        public static Form1 MainForm;
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainForm = new Form1();
+            Application.Run(MainForm);
+        }
+    }
+}
