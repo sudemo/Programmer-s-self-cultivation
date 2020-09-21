c9bb97e15cd911556d6a37cff2bc94a56a53d6cf
neo <1733112322@qq.com>
2020-09-21 08:54:22 +0800

    Chartdemo1

---
+++ C#/testdemo2/chartdemo/Chartdemo1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Chartdemo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(177, 42);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "示例1";
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(558, 396);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ C#/testdemo2/chartdemo/Chartdemo1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Chartdemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<int> x = new List<int>() { 1, 2, 3, 4 };
+            List<int> y1 = new List<int>() { 10,20,60,31};
+            List<int> y2 = new List<int>() { 20, 20, 30, 31 };
+           
+            //chart1.Series["示例1"].Points.DataBindXY(x, y1);
+            chart1.Series["Series2"].Points.DataBindXY(x,y2);
+
+           // chart1.Series.Add(new Series()); //添加一个图表序列
+           // ct.Series[0].XValueType = ChartValueType.String; //设置X轴上的值类型
+            chart1.Series[0].Label = "#VAL"; //设置显示X Y的值
+            chart1.Series[0].ToolTip = "#VALX列\r#VAL"; //鼠标移动到对应点显示数值
+            //chart1.Titles = "";
+            //chart1.Series[0].ChartArea = chart1.ChartAreas[0].Name; //设置图表背景框ChartArea 
+            //chart1.Series[0].ChartType = SeriesChartType.Line; //图类型(折线)
+            //chart1.Series[0].ChartType = SeriesChartType.Column;
+            chart1.Series[0].Points.DataBindXY(x, y1); //添加数据
+
+
+            Series newSeries = new Series("SeriesDemo");
+            newSeries.ChartType = SeriesChartType.Line;
+            newSeries.BorderWidth = 2;
+            newSeries.Color = Color.OrangeRed;
+            newSeries.XValueType = ChartValueType.DateTime;
+            chart1.Series.Add(newSeries);
+            chart1.Series["SeriesDemo"].Points.DataBindXY(x, y2);
+            chart1.Series[2].ChartType = SeriesChartType.Line;
+
+        }
+    }
+}
