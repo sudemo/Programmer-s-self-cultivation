84b76e45e33343498c9ea27f26f24d5815f252fa
neo <1733112322@qq.com>
2020-09-27 08:45:18 +0800

    chartdemo2

---
+++ C#/testdemo2/chartdemo/chartdemo2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace chartdemo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.startTrendingButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(800, 450);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // startTrendingButton
+            // 
+            this.startTrendingButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.startTrendingButton.AutoSize = true;
+            this.startTrendingButton.Location = new System.Drawing.Point(667, 160);
+            this.startTrendingButton.Name = "startTrendingButton";
+            this.startTrendingButton.Size = new System.Drawing.Size(133, 57);
+            this.startTrendingButton.TabIndex = 1;
+            this.startTrendingButton.Text = "startTrendingButton";
+            this.startTrendingButton.UseVisualStyleBackColor = true;
+            this.startTrendingButton.Click += new System.EventHandler(this.startTrending_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.StopButton.Location = new System.Drawing.Point(667, 249);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(133, 51);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "StopButton";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.stopTrending_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.startTrendingButton);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button startTrendingButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ C#/testdemo2/chartdemo/chartdemo2/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace chartdemo2
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        private Thread addDataRunnerThread;
+        private Random rand = new Random();
+        //private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        public delegate void AddDataDelegate(); //定义一个委托
+        public AddDataDelegate addDataDel;   //定义一个委托变量
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+
+        //    List<int> x = new List<int>() { 1, 2, 3, 4 };
+        //    List<int> y1 = new List<int>() { 100, 20, 60, 31 };
+        //    List<int> y2 = new List<int>() { 200, 20, 30, 31 };
+
+        //    //chart1.Series["示例1"].Points.DataBindXY(x, y1);
+        //    //chart1.Series[0].Points.DataBindXY(x, y2);
+            
+        //}
+
+    
+
+    
+
+
+    private void Form1_Load(object sender, System.EventArgs e)
+    {
+
+        // create the Adding Data Thread but do not start until start button clicked
+        ThreadStart addDataThreadStart = new ThreadStart(AddDataThreadLoop);
+        addDataRunnerThread = new Thread(addDataThreadStart);
+        addDataRunnerThread.IsBackground = true; //后台
+                                                 // create a delegate for adding data
+        //addDataDel += new AddDataDelegate(AddData);  //注册一个方法，初始化一个委托变量
+        addDataDel += AddData;//另一种初始化方法
+    }
+
+    private void startTrending_Click(object sender, System.EventArgs e)
+    {
+        // Disable all controls on the form
+        startTrendingButton.Enabled = false;
+        // and only Enable the Stop button
+        StopButton.Enabled = true;
+
+        // Predefine the viewing area of the chart
+        var  minValue = DateTime.Now;
+        var  maxValue = minValue.AddSeconds(120);
+
+        chart1.ChartAreas[0].AxisX.Minimum = minValue.ToOADate();
+        chart1.ChartAreas[0].AxisX.Maximum = maxValue.ToOADate();
+
+        // Reset number of series in the chart.
+        chart1.Series.Clear();
+
+        // create a line chart series
+        Series newSeries = new Series("Series1");
+        newSeries.ChartType = SeriesChartType.Line;
+        newSeries.BorderWidth = 2;
+        newSeries.Color = Color.OrangeRed;
+        newSeries.XValueType = ChartValueType.DateTime;
+        chart1.Series.Add(newSeries);
+
+
+        // start worker threads.
+        if (addDataRunnerThread.IsAlive == true)
+        {
+            addDataRunnerThread.Resume();
+        }
+        else
+        {
+            addDataRunnerThread.Start();
+        }
+
+    }
+
+        [Obsolete]  // shawanyi 
+        private void stopTrending_Click(object sender, System.EventArgs e)
+    {
+        if (addDataRunnerThread.IsAlive == true)
+        {
+            addDataRunnerThread.Suspend();
+        }
+
+            // Enable all controls on the form
+            startTrendingButton.Enabled = true;
+            // and only Disable the Stop button
+            StopButton.Enabled = false;
+    }
+
+    /// Main loop for the thread that adds data to the chart.
+    /// The main purpose of this function is to Invoke AddData
+    /// function every 1000ms (1 second).
+    private void AddDataThreadLoop()
+    {
+        while (true)
+        {
+            chart1.Invoke(addDataDel);
+
+            Thread.Sleep(100);
+        }
+    }
+
+    public void AddData()
+    {
+        DateTime timeStamp = DateTime.Now;
+
+        foreach (Series ptSeries in chart1.Series)
+        {
+            AddNewPoint(timeStamp, ptSeries);
+        }
+    }
+
+    /// The AddNewPoint function is called for each series in the chart when
+    /// new points need to be added.  The new point will be placed at specified
+    /// X axis (Date/Time) position with a Y value in a range +/- 1 from the previous
+    /// data point's Y value, and not smaller than zero.
+    public void AddNewPoint(DateTime timeStamp, System.Windows.Forms.DataVisualization.Charting.Series ptSeries)
+    {
+        double newVal = 0;
+
+        if (ptSeries.Points.Count > 0)
+        {
+            newVal = ptSeries.Points[ptSeries.Points.Count - 1].YValues[0] + ((rand.NextDouble() * 2) - 1);
+        }
+
+        if (newVal < 0)
+            newVal = 0;
+
+        // Add new data point to its series.
+        ptSeries.Points.AddXY(timeStamp.ToOADate(), rand.Next(10, 20));
+
+        // remove all points from the source series older than 1.5 minutes.
+        double removeBefore = timeStamp.AddSeconds((double)(90) * (-1)).ToOADate();
+        //remove oldest values to maintain a constant number of data points
+        while (ptSeries.Points[0].XValue < removeBefore)
+        {
+            ptSeries.Points.RemoveAt(0);
+        }
+
+        chart1.ChartAreas[0].AxisX.Minimum = ptSeries.Points[0].XValue;
+        chart1.ChartAreas[0].AxisX.Maximum = DateTime.FromOADate(ptSeries.Points[0].XValue).AddMinutes(2).ToOADate();
+
+        chart1.Invalidate();
+    }
+       
+
+    }
+}
