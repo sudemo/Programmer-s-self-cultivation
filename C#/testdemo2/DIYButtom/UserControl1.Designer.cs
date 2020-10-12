a512615528d7734f12919c406e72ed4c70cdaa84
neo <1733112322@qq.com>
2020-10-12 11:31:50 +0800

    DIYButton

---
+++ C#/testdemo2/DIYButtom/UserControl1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DIYButtom
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::DIYButtom.Properties.Resources.kaiguanguan_1;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(145, 109);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ C#/testdemo2/DIYButtom/UserControl1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Resources;
+namespace DIYButtom
+{
+    public partial class UserControl1: UserControl
+    {
+
+        bool isCheck = true;
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Bitmap bitMapOn = null;
+            Bitmap bitMapOff = null;
+
+           
+            bitMapOn = Properties.Resources.kaiguanguan_1;
+            bitMapOff = Properties.Resources.kaiguanguan_copy_copy;
+
+            Graphics g = e.Graphics;
+            Rectangle rec = new Rectangle(0, 0, Size.Width/2, this.Size.Height);
+
+            if (isCheck)
+            {
+                //g.
+                g.DrawImage(bitMapOn, rec);
+            }
+            else
+            {
+                g.DrawImage(bitMapOff, rec);
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            isCheck = !isCheck;
+            this.Invalidate();
+            //Console.WriteLine(Application.StartupPath);
+           // pictureBox1.Image = Properties.Resources.kaiguanguan_copy_copy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.kaiguanguan_1;
+         }
+    }
+}
