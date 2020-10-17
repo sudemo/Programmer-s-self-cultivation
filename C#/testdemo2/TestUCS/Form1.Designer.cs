528fae3cd83e3e36bd67d698cf1ecb698900f819
neo <1733112322@qq.com>
2020-10-17 13:58:12 +0800

    test ucs

---
+++ C#/testdemo2/TestUCS/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace TestUCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucSwitch1 = new UCS.UCSwitch();
+            this.SuspendLayout();
+            // 
+            // ucSwitch1
+            // 
+            this.ucSwitch1.Checked = false;
+            this.ucSwitch1.FalseColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(189)))), ((int)(((byte)(189)))));
+            this.ucSwitch1.FalseTextColr = System.Drawing.Color.White;
+            this.ucSwitch1.Location = new System.Drawing.Point(87, 55);
+            this.ucSwitch1.Name = "ucSwitch1";
+            this.ucSwitch1.Size = new System.Drawing.Size(92, 35);
+            this.ucSwitch1.SwitchType = UCS.SwitchType.Quadrilateral;
+            this.ucSwitch1.TabIndex = 1;
+            this.ucSwitch1.Texts = null;
+            this.ucSwitch1.TrueColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(59)))));
+            this.ucSwitch1.TrueTextColr = System.Drawing.Color.White;
+            this.ucSwitch1.CheckedChanged += new System.EventHandler(this.ucSwitch1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ucSwitch1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        //private DIYButtom.BenNHControl benNHControl1;
+        private UCS.UCSwitch ucSwitch1;
+    }
+}
+
---
+++ C#/testdemo2/TestUCS/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestUCS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ucSwitch1_CheckedChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Test");
+            var stringFormat = new StringFormat();
+            stringFormat.Alignment = StringAlignment.Center;	//居中字符串【左、右对齐类似】
+            Graphics graphics = this.CreateGraphics();
+            graphics.DrawString("hello", Font, new SolidBrush(Color.Green), new Point(20,5), stringFormat);
+            //g.DrawString("hello", Font, new SolidBrush(m_trueTextColr), new Point((Height - 2 - 4 - 10) / 2, intTextY));
+        }
+    }
+}
